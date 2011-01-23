01da14887fdfc9bfcebdb040ba97d99df79dbea8
Ivan Simonini <ivan.simonini@roundhousecode.com>
2011-01-23 13:28:44 +0100

    Consistent sources.

--- d.php
+++ d.php
@@ -95,13 +95,7 @@
 	}
 	$parsed = strtolower(implode('/', $token).'/');
 
-	$cats = array (
-		'/tru\/naluten\//' => array ('tru/', 'tru/primo/', 'tru/secondo/'),
-		'/nanowrimo\/corvino\/multicolore\//' => array ('nano/corvino/'),
-		'/storie\/2010\//' => array ('str/2010/'),
-		'/storie\/2011\//' => array ('str/2011/'),
-		'/storie\//' => array ('str/', 'str/2010/', 'str/2011/')
-	);
+	require_once ('sources.php');
 	
 	$section = false;
 	$location = false;
--- sources.php
+++ sources.php
@@ -1,47 +1,9 @@
 <?php
-
-	$categories['Tru'] = new Category (
-		'/Tru\/Naluten\//',
-		array ('tru/', 'tru/primo/', 'tru/secondo/'),
-		'Tru/Naluten/',
-		'Tru Naluten'
-	);
-	$categories['NaNoWriMo'] = new Category (
-		'/NaNoWriMo\//',
-		array ('nano/'),
-		'NaNoWriMo/',
-		'NaNoWriMo',
-		false,
-		array('2010' => new Category (
-			'/2010\//',
-			array ('nano/2010/'),
-			'2010/',
-			'2010',
-			array ('ext' => 'pdf',
-				'mime' => 'application/pdf')),
-		new Category (
-			'/Corvino\/Multicolore\//',
-			array('nano/corvino/'),
-			'Corvino/Multicolore/',
-			'Corvino Multicolore'
-		))
-	);
-	$categories['Storie'] = new Category (
-		'/Storie\//',
-		array ('str/', 'str/2010/', 'str/2011/'),
-		'Storie/',
-		'Storie',
-		false,
-		array('2010' => new Category (
-			'/2010\//',
-			array ('str/2010/'),
-			'2010/',
-			'2010'
-		), '2011' => new Category (
-			'/2011\//',
-			array ('str/2011/'),
-			'2011/',
-			'2011'
-		))
+	$cats = array (
+		'/tru\/naluten\//' => array ('tru/', 'tru/primo/', 'tru/secondo/'),
+		'/nanowrimo\/corvino\/multicolore\//' => array ('nano/corvino/'),
+		'/storie\/2010\//' => array ('str/2010/'),
+		'/storie\/2011\//' => array ('str/2011/'),
+		'/storie\//' => array ('str/', 'str/2010/', 'str/2011/')
 	);
 ?>
