f1edd588ec784aef307079af21c1e65ff846cab2
Ivan Simonini <ivan.simonini@roundhousecode.com>
2011-06-15 16:15:26 +0200

    Film/games/stuff reviews.

---
+++ film/classifiche.php
@@ -0,0 +1,50 @@
+<?php
+	$title=array('Classifica','Cose belle, cose brutte');
+	function mkpage($d){
+?>
+<div class="small">
+	<div class="section">
+		<h2>
+			Fucking awesome!!!
+		</h2><p>
+			Questi sono decisamente da vedere/provare/comprare.
+		</p><ul><li>
+				<?=$d->link('Recensioni/AC-Brotherhood/','Assassin&apos;s Creed Brotherhood')?>
+			</li></ul>
+		</p>
+	</div><div class="section">
+		<h2>
+			Si lascia guardare
+		</h2><p>
+			Questi si lasciano guardare. Con le dovute precauzioni.
+		</p><ul><li>
+				<?=$d->link('Recensioni/Masterforce/','Masterforce')?>
+		</li></ul>
+	</div><div class="section">
+		<h2>
+			Meh. / Mah
+		</h2><p>
+			Questi non sono per tutti, sono per chi ha gusti particolari o lo
+			stomaco di ferro.
+		</p><ul><li>
+				<?=$d->link('Recensioni/Robocop/','Robocop')?>
+			</li><li>
+				<?=$d->link('Recensioni/PaniPoniDash/','Pani Poni Dash')?>
+		</li></ul>
+	</div><div class="section">
+		<h2>
+			L'ha fatto Micheal Bay?
+		</h2><ul><li>
+			<?=$d->link('Recensioni/Blassreiter/','Blassreiter');?>
+		</li></ul>
+	</div><div class="section">
+		<h2>
+			No
+		</h2><p>
+			Questi no.
+		<p><ul><li>
+				<?=$d->link('Recensioni/Thor/','Thor')?>
+		</li></ul>
+	</div>
+</div>
+<?php } ?>
---
+++ film/index.php
@@ -0,0 +1,54 @@
+<?php
+	$title=array('Recensioni','Sincere, spesso brutte');
+	function mkpage($d) {
+?>
+<div class="small">
+	<div class="section">
+		<p>
+			Non ne posso più, devo assolutamente sfogarmi.
+		</p><p>
+			Ma prima qualche parola per spiegare questa sezione.
+		</p>
+	</div><div class="section">
+		<p>
+			Non a caso, quell'arguto professore ch'è War arrivò un giorno e mi
+			disse:
+		</p><div class="outside"><p>
+				«Gods, è normale che non ci sia scritto niente, qui?»
+		</p></div><p>
+			Ed io risposì ch'era tutto come previsto.
+		</p>
+	</div><div class="section">
+		<p>
+			Insomma, non è che una sezione appena nata meriti subito
+			un'introduzione, no? Non necessariamente. Mettiamola così: era
+			scrittura virale, come quelle pubblicità che non si capiscono
+			affatto, ma dicono soltanto un nome...
+		</p><p>
+			Non che una pagina intitolata ‘Recensioni’ fosse 'sto gran
+			mistero...
+		</p><p>
+			Comunque sia, ecco le mie
+		</p><h2>
+			Recensioni
+		</h2><p>
+			Perché come ho dimostrato <a href="Storie/2011/XXXVIII/">qui</a>, le
+			storie non sono adatte a recensire... Ecco dunque uno spazio
+			dedicato.
+		</p>
+	</div><div class="section">
+		<p>
+			Dedicato quasi esclusivamente a recensioni negative, ovviamente.
+			Come ci dimostrano amici come l'AVGN, quel tizio con gli occhiali e
+			MovieBob, l'industria del cinema produce immense quantità di
+			schifezza.
+		</p><p>
+			Ma non è solo colpa loro, e infatti non mi limiterò a recensire
+			film, qui. Se ci sono cose di cui posso definirmi appassionato, ci
+			sono anche i videogiochi, i Gundam, i Transformers e i fumetti.
+		</p><p>
+			Con il tempo, spalerò merda un po' su tutto.
+		</p>
+	</div>
+</div>
+<?php } ?>
