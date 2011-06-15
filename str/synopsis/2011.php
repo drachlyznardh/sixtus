96d5a2fd5c2e9e106e7b459b98ff50beaf40638a
Ivan Simonini <ivan.simonini@roundhousecode.com>
2011-06-15 16:15:54 +0200

    Stories synopsis.

---
+++ str/synopsis/2011.php
@@ -0,0 +1,121 @@
+<div class="small">
+	<div class="section">
+		<p>
+			E così venne il 2011...
+		</p><p>
+			E forse c'è spazio, qui, per qualche considerazione sull'anno
+			passato. Ah, l'anno passato. Quelli ch'han letto le storie
+			precedenti potrebbero capire.
+		</p><p>
+			Inizio quest'anno con una ritrovata libertà sentimentale (lol,
+			grande consolazione, grande fuga), senza un nuovo Gundam,
+			prossimamente con un fratello in più, con una lunga fila di
+			esami da dare e nessuna voglia di lavorare.
+		</p>
+	</div>
+</div><div class="wider">
+	<div class="widecontent">
+		<div class="section">
+			<p id="2011">
+				Scriverò quando ci sarà qualcosa da scrivere.
+			</p>
+		</div><div class="section">
+			<p>
+				Pare che tuttora non ci sia nulla da scriverci,
+				qui. Farò un'altra volta...
+			</p>
+		</div>
+	</div><div class="widelist">
+		<div class="section">
+			<h2 class="reverse">
+				Storie 2011
+			</h2><ol start="18">
+				<li><?=$d->link('Storie/2011/XVIII/', 'Liber Javae')?></li>
+				<li><?=$d->link('Storie/2011/XIX/', 'Permessi')?></li>
+				<li><?=$d->link('Storie/2011/XX/', 'Guida')?></li>
+				<li><?=$d->link('Storie/2011/XXI/','Programmazione 40.000')?></li>
+				<li><?=$d->link('Storie/2011/XXII/', 'Tecniche avanzate')?></li>
+				<li><?=$d->link('Storie/2011/XXIII/', 'Cose complicate')?></li>
+				<li><?=$d->link('Storie/2011/XXIV', 'Il futuro')?></li>
+				<li><?=$d->link('Storie/2011/XXV/', 'No')?></li>
+				<li><?=$d->link('Storie/2011/XXVI/', 'La sconfitta')?></li>
+				<li><?=$d->link('Storie/2011/XXVII/', 'LaTeX')?></li>
+			</ol>
+		</div>
+	</div>
+<div class="section">
+	<p>
+		Ma poi cominciano a comparire delle persone, il cui effetto - almeno
+		per quanto mi riguarda, non è affatto trascurabile.
+	</p>
+</div><div class="wider">
+	<div class="widecontent">
+		<div class="section">
+			<h2>
+				Il Magnetismo
+			</h2><p>
+				Era tempo che mi mettessi a scrivere questa sezione, immagino...
+				Le mie idee non sono affatto chiaro, si vedano appunto le storie
+				di questo arco; per adesso mi sento di dire che le cose non
+				vanno come prima e probabilmente qualcos'altro cambiarà prima
+				che la storia finisca. Boh.
+			</p>
+		</div>
+	</div><div class="widelist">
+		<div class="section">
+			<h2 class="reverse">
+				Il Magnetismo
+			</h2><ol start="28">
+				<li><?=$d->link('Storie/2011/XXVIII/','L&apos;odio')?></li>
+				<li><?=$d->link('Storie/2011/XXIX/','Il Magnete')?></li>
+				<li><?=$d->link('Storie/2011/XXX/','L&apos;interpretazione')?></li>
+			</ol>
+		</div>
+	</div>
+</div><div class="section">
+	<p>
+		E dopo un po' di tempo durante il quale non accadde nulla di nulla...
+	</p>
+</div><div class="wider">
+	<div class="widecontent">
+		<div class="section">
+			<h2>
+				Primavera 2011
+			</h2><p>
+				Ora che ho quasi battuto la <span class="magnet">Ragazza
+				Magnete</span> la mia vita può andare avanti. Cosa farò adesso?
+			</p>
+		</div><div class="section">
+			<p>
+				Beh, dirò che molte cose hanno semplicemente smesso di capitare:
+				la <span class="magnet">Ragazza Magnete</span> non compare quasi
+				più, $rossa e le sue amiche-radar non frequentano più il bar...
+				devo forse passare a fonti d'odio alterntative?
+			</p>
+		</div>
+	</div><div class="widelist">
+		<div class="section">
+			<h2 class="reverse">
+				Primavera 2011
+			</h2><ol start="31" style="padding-left: 5em">
+				<li><?=$d->link('Storie/2011/XXXI/','Deus Ex Machina')?></li>
+				<li><?=$d->link('Storie/2011/XXXII/','Dottor Odio')?></li>
+				<li><?=$d->link('Storie/2011/XXXIII/','L&apos;inaspettata')?></li>
+				<li><?=$d->link('Storie/2011/XXXIV/','Considerazioni')?></li>
+				<li><?=$d->link('Storie/2011/XXXV/','Accuse')?></li>
+				<li><?=$d->link('Storie/2011/XXXVI/','Quel tizio')?></li>
+				<li><?=$d->link('Storie/2011/XXXVII/','PHP')?></li>
+				<li><?=$d->link('Storie/2011/XXXVIII/','Raccontare')?></li>
+				<li><?=$d->link('Storie/2011/XXXIX/','Forever Alone')?></li>
+				<li><?=$d->link('Storie/2011/XL/','Domanda&amp;Offerta')?></li>
+				<li><?=$d->link('Storie/2011/XLI/','Raccontare II')?></li>
+				<li><?=$d->link('Storie/2011/XLII/','La vecchiaia')?></li>
+				<li><?=$d->link('Storie/2011/XLIII/','Gli anni')?></li>
+				<li><?=$d->link('Storie/2011/XLIV/','Pillole')?></li>
+				<li><?=$d->link('Storie/2011/XLV/','Wireless')?></li>
+				<li><?=$d->link('Storie/2011/XLVI/','L&apos;uomo con due ombrelli')?></li>
+				<li><?=$d->link('Storie/2011/XLVII/','Impresa (II)')?></li>
+			</ol>
+		</div>
+	</div>
+</div>
