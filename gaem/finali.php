cd7f1ffa636f88c72cd8fe1e52da57a3c13e601c
Ivan Simonini <lyznardh@Tanngrisnir.(none)>
2011-10-26 17:52:33 +0200

    Ah! Interactive story!

---
+++ gaem/finali.php
@@ -0,0 +1,63 @@
+ <?php
+	$title=array('La storia interattiva','I finali');
+	function mkpage($d){
+?>
+<div class="wider">
+	<div class="widelist">
+		<div class="section">
+			<h2>
+				Finali
+			</h2><p>
+				<?=$d->link('Gaem/Finali/','Questa è la fine', 'I')?>
+			</p><ol><li id="li-f1">
+					A nessuno fregò
+				</li><li id="li-f2">
+					Tutto a puttane
+			</li></ol>
+		</div>
+	</div><div class="widecontent">
+		<div class="tab" id="tab-i">
+			<div class="section">
+				<p>
+					Complimenti. Sei arrivato in fondo alla storia interattiva.
+				</p><p>
+					Cioè, no, peccato. Sei morto o sei uscito dalla storia.
+					Poverello. Tecnicamente, infatti, hai perso: non è questo il
+					finale buono.
+				</p><p>
+					Di fatto, questa è la pagina per i finali marci, quelli che
+					corrispondono alle scelte sbagliate.
+				</p>
+			</div>
+		</div><div class="tab" id="tab-f1">
+			<div class="section">
+	<p>
+		Eh, peccato. Pare che al lettore non freghi un cazzo.
+	</p>
+</div><div class="section">
+	<p>
+		Ma il GODS, che non aveva altro interesse se non il mutante aspetto
+		delle nuvole, rispose
+	</p><div class="outside"><p>
+			«<span class="gods">Messer Bolo, forse non vedi che sono impegnato a
+			rimestare le mie scaroffie?</span>» rimestano le sue scartofie, e
+			poi «<span class="gods">Vado ora a raccattare due cari amici,
+			Giorgio &amp; Piero, abbiamo una cosa da cercare</span>» e partì.
+	</p></div><p>
+		Il resto è <a
+		href="http://www.youtube.com/results?search_query=giorgio+e+piero">questo</a>.
+		FINE.
+	</p>
+			</div>
+		</div><div class="tab" id="tab-f2">
+			<div class="section">
+	<p>
+		Una volta radunate quindici puttane, tutto andò a posto.
+	</p><p>
+		Tutti vissero felici e contenti, tranne qualcuno che morì. FINE.
+	</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php } ?>
---
+++ gaem/index.php
@@ -0,0 +1,149 @@
+<?php
+	$title=array('La storia interattiva','Ma solo se volete');
+	function mkpage($d){
+		$self='Gaem/';
+		$final='Gaem/Finali/';
+?>
+<div class="wider">
+	<div class="widelist">
+		<div class="section">
+			<h2>
+				Gaem
+			</h2><ol><li id="li-i">
+					<?=$d->link($self,'Intro','I')?>
+				</li><li id="li-start">
+					<?=$d->link($self,'Start','START')?>
+			</li></ol>
+		</div>
+	</div><div class="widecontent">
+		<div class="tab" id="tab-i">
+			<div class="section">
+	<p>
+		Si può fare.
+	</p><p>
+		Ed è un mostro.
+	</p><p>
+		Comincia <?=$d->link($self,'qui','START')?>.
+	</p>
+			</div>
+		</div><div class="tab" id="tab-start">
+			<div class="section">
+	<p>
+		C'era una volta il <span class="em">GODS</span>, che se ne andava a
+		spasso, bel bello, per il suo regno. Perché lui poteva.
+	</p><p>
+		Perché il regno era suo.
+	</p>
+</div><div class="section">
+	<p>
+		Incappò un bel di il GODS in uno dei suoi cavalieri, il <span
+		class="em">Bolo</span>.
+	</p><div class="outside"><p>
+			Il Bolo parlò e disse«<span class="bolo">Maestà! Proprio voi
+			cercavo</span>»
+		</p><p>
+			«<span class="gods">Messer Bolo!</span>» rispose il GODS «<span
+			class="gods">Qual buon vento?</span>»
+		</p><p>
+			«<span class="bolo">Un'impresa, sire! Un'impresa!</span> annunciò il
+			cavaliere «<span class="bolo">Il popolo necessita di storie</span>»
+			ma poi, rannuvolandosi in volto «<span class="bolo">Ma devo anche
+			annunciarvi una notizia terribile: si tratta del pentolone delle
+			storie. È vuoto, mio signore!</span>»
+	</p></div>
+</div><div class="section">
+	<h2>
+		Scelta
+	</h2><p>
+		È arrivato il momento di scegliere. Scegli.
+	</p><ul><li>
+			<?=$d->link($final,'Non mi frega un cazzo di questa storia','F1')?>
+		</li><li>
+			<?=$d->link($self,'Oh no! Il pentolone?!?','S1')?>
+	</li></ul>
+			</div>
+				</div><div class="tab" id="tab-s1">
+			<div class="section">
+	<div class="outside"><p>
+			«<span class="gods">Oh no! Il pentolone!</span>» esclamò il GODS.
+		</p><p>
+			Puntando il dito, ordinò «<span class="gods">Presto! Alla
+			pentol-caverna!</span>»
+	</p></div><p>
+		Tarananananananananana!
+	</p>
+</div><div class="section">
+	<p>
+		La <span class="em">Pentol-caverna</span> era un luogo remoto e
+		incantato, dimora nel misterioso fabbro nano <span
+		class="em">Dacav</span>, dall'orrido aspetto.
+	</p><p>
+		Sbuffando dalle enormi froge, il nano si rivolse al GODS, che bussava
+		alla sua porta.
+	</p><div class="outside"><p>
+			«<span class="dacav">Sire! Sire! Terribile! Terribilissimo!</span>»
+			piagnucolava il fabbro «<span class="dacav">Il pentolone s'è
+			esaurito! Non era mai successo, in 30000000000000000000&pi;²³anni!</span>»
+		</p><p>
+			«<span class="dacav">Me lo ricordo, io c'ero</span>» aggiunse il nano,
+			ch'era più vecchio delle fondamenta della Terra.
+	</p></div><p>
+		Atterrito, il GODS visionò il contenuto del Pentolone. Era vuoto.
+	</p><p>
+		Senza le storie estratte dal pentolone, il regno sarebbe andato a
+		puttane! Come fare? Se tutto il regno fosse andato a puttane, quante
+		puttane ci sarebbero volute?
+	</p>
+</div><div class="section">
+	<h2>
+		Scelta
+	</h2><ul><li>
+			<?=$d->link($final,'Non sono… 15?','F2')?>
+		</li><li>
+			<?=$d->link($self,'Almeno il doppio','S2')?>
+	</li></ul>
+			</div>
+		</div><div class="tab" id="tab-s2">
+			<div class="section">
+	<div class="outside"><p>
+			«<span class="gods">Impossibile!</span>» pianse il GODS, non sapendo
+			che fare «<span class="gods">Dove troveremo tutte queste
+			puttane?</span>»
+	</p></div><p>
+		Alla ricerca disperata d'una soluzione.
+	</p><div class="outside"><p>
+			«<span class="war">Non disperate, mio signore!</span>» esclamò
+			allora <span class="em">War</span>, il mago di corte.
+			«<span class="war">Basterà riempire il Pentolone con nuove
+			storie!</span>» millantò.
+	</p></div><p>
+		Ed ecco che immediatamente il mago si mise al lavoro su un <span
+		class="code">algoritmo intelligente [proattivo &amp; basculante]
+		neutrinico ad induzione protonica brandeggiabile di classe 7</span> per
+		rivelare la concentrazione di particelle di storia
+		nell'aria.
+	</p><p>
+		Così armati, i nostri eroi partirono alla ricerca di storie!
+	</p>
+</div><div class="section">
+	<p>
+		Quella sera, all'ombra d'un castagno matto, udirono in lontananza una
+		voce.
+	</p><div class="outside"><p>
+			«<span class="mitch">Amici! Amici!</span>» esclamò la voce «<span
+			class="mitch">Eccomi! Seppur in ritardo, vi raggiungo!</span>»
+	</p></div><p>
+		Era <span class="em">Mitch</span> l'erborista, di ritorno dalla sua
+		personale impresa, la ricerca della ricerca.
+	</p><p>
+		Si narrava infatti di un introvabile maestro erborista, sommo maestro,
+		che aveva due laboratori, uno in collina e uno in pianura. E pareva
+		trovarsi sempre nell'altro.
+	</p><p>
+		<span class="em forte">Continua…</span>
+	</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php } ?>
