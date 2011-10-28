300e30cb799c2f665bc3268d23696b400613411a
DrachLyznardh <drachlyznardh@roundhousecode.com>
2011-10-28 14:13:50 +0200

    News sides changed a bit.

---
+++ tmpl/side/new.2011.php
@@ -0,0 +1,11 @@
+<div class="section">
+	<h2>
+		Notizie 2011
+	</h2><p>
+		<?=$d->link('News/2011/Lug/','Luglio 2011')?>
+	</p><p>
+		<?=$d->link('News/2011/Ago/','Agosto 2011')?>
+	</p><p>
+		<?=$d->link('News/2011/Set/','Settembre 2011')?>
+	</p>
+</div>
--- tmpl/side/new.php
+++ tmpl/side/new.php
@@ -2,17 +2,20 @@
 	<h2>
 		Novità
 	</h2><p>
+		<span class="em">27/10</span> –
+		Gundam AGE [<?=$d->link('Recensioni/Show/XIV/', 'ep2~3', 'II')?>]
+	</p><p>
 		<span class="em">23/10</span> –
-		L'Ordine della Fenice (<?=$d->link('Recensioni/HarryPotter/', '3', 'V3')?>)
+		L'Ordine della Fenice (<?=$d->link('Recensioni/Libri/XII/', '3', 'V3')?>)
 	</p><p>
 		<span class="em">18/10</span> –
 		<?=$d->link('Storie/2011/LXVII/', 'Ancora ce la posso fare', 'INTRO')?>
 	</p><p>
 		<span class="em">16/10</span> –
-		<?=$d->link('Recensioni/TransFormersPrime/','TransFormers: Prime','INTRO')?>
+		<?=$d->link('Recensioni/Show/XV/','TransFormers: Prime','INTRO')?>
 	</p><p>
 		<span class="em">15/10</span> –
-		<?=$d->link('Recensioni/KamenRider/','Let&apos;go Kamen Riders','M2011')?>
+		<?=$d->link('Recensioni/Show/XIII/','Let&apos;go Kamen Riders','M2011')?>
 	</p><p>
 		<span class="em">14/10</span> –
 		Il 13 ottobre [Bonus
@@ -23,25 +26,13 @@
 		<?=$d->link('Storie/2011/LXV/','Il 13 ottobre','INTRO')?>
 	</p><p>
 		<span class="em">12/10</span> –
-		L'altro <?=$d->link('Recensioni/HarryPotter/','Ordine della Fenice','V2')?>
+		L'altro <?=$d->link('Recensioni/Libri/XII/','Ordine della Fenice','V2')?>
 	</p><p>
 		<span class="em">12/10</span> – 
-		<?=$d->link('Recensioni/GundamAGE/','Gundam AGE','INTRO')?>
+		<?=$d->link('Recensioni/Show/XIV/','Gundam AGE','INTRO')?>
 	</p><p>
 		<span class="em">03/10</span> –
 		<?=$d->link('Storie/2011/LXIV/','La metafora','INTRO')?>
-	</p><p>
-		<span class="em">02/10</span> –
-		<?=$d->link('Extra/Record/','Record','INTRO')?>
-	</p>
-</div><div class="section">
-	<h2>
-		Archivio
-	</h2><p>
-		<?=$d->link('News/2011/Lug/','Luglio 2011')?>
-	</p><p>
-		<?=$d->link('News/2011/Ago/','Agosto 2011')?>
-	</p><p>
-		<?=$d->link('News/2011/Set/','Settembre 2011')?>
 	</p>
 </div>
+<?php require_once('new.2011.php'); ?>
