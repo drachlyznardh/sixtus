8c62cc635bd1252d4a9fb8bcf9cec49715644702
DrachLyznardh <drachlyznardh@roundhousecode.com>
2011-10-28 14:17:28 +0200

    New sides for review category (and children).

---
+++ tmpl/side/review.game.php
@@ -0,0 +1,9 @@
+<div class="section">
+	<h2 class="reverse">
+		Giochi
+	</h2><p>
+			<?=$d->link('Recensioni/Giochi/II/', 'Assassin&apos;s Creed
+			Brotherhood')?> – 2010
+	</p>
+</div>
+<?php require_once ('review.php'); ?>
---
+++ tmpl/side/review.php
@@ -0,0 +1,55 @@
+<div class="section">
+	<h2>
+		Recensioni
+	</h2><ol><li>
+			<?=$d->link('Recensioni/Film/I/','Thor')?> - 2011
+		</li><li>
+			<?=$d->link('Recensioni/Giochi/II/','AC Brotherhood')?> - 2010
+		</li><li>
+			<?=$d->link('Recensioni/Film/III/','Robocop')?>
+			<?=$d->link('Recensioni/Film/III/#II','1')?>,
+			<?=$d->link('Recensioni/Film/III/#VI','2')?>  &amp;
+			<?=$d->link('Recensioni/Film/III/#X','3')?> - '87,'90,'93
+		</li><li>
+			<?=$d->link('Recensioni/Show/IV/','Pani Poni Dash')?> – 2005
+		</li><li>
+			<?=$d->link('Recensioni/Show/V/','BlassReiter')?> – 2008
+		</li><li>
+			<?=$d->link('Recensioni/Show/VI/','Masterforce')?> – 1988
+		</li><li>
+			<?=$d->link('Recensioni/Film/VII/','Dark Of The Moon')?> – 2011
+		</li><li>
+			<?=$d->link('Recensioni/Film/VIII/','Capitan America')?> – 2011
+		</li><li>
+			<?=$d->link('Recensioni/Show/IX/','Angel Beats!')?> – 2010
+		</li><li>
+			<?=$d->link('Recensioni/Film/X/','I Puffi')?> – 2011
+		</li><li>
+			<?=$d->link('Recensioni/Libri/XI/','Nessun Dove')?> – 1996
+		</li><li>
+			<?=$d->link('Recensioni/Libri/XII/','Harry Potter','INTRO')?> – 1997~2007
+		</li><li>
+			<?=$d->link('Recensioni/Show/XIII/','Kamen Rider')?> – 1971~2011
+		</li><li>
+			<?=$d->link('Recensioni/Show/XIV/','Gundam AGE','INTRO')?> – 2011
+		</li><li>
+			<?=$d->link('Recensioni/Show/XV/',
+				'TransFormers: Prime', 'INTRO')?> – 2010~2011
+		</li><li>
+			<?=$d->link('Recensioni/Film/Brutti/XVI/', 'Bitch Slap', '')?> – 2009
+	</li></ol>
+</div><div class="section">
+	<h2>
+		Work in Progress
+	</h2><p>
+		Harry Potter &amp; il Principe Mezzosangue
+	</p><p>
+		Pirate Sentai Gokaiger
+	</p><p>
+		Kamen Rider Fourze
+	</p><p>
+		Doctor Who s01 (2005)
+	</p><p>
+		Gundam AGE
+	</p>
+</div>
