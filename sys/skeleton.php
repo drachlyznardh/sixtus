835c54483e64f794b6c15ad66b571f5e3006a798
DrachLyznardh <drachlyznardh@roundhousecode.com>
2012-05-02 11:44:38 +0200

    GoogleAnalytics has been added. For the evil empire!

--- sys/skeleton.php
+++ sys/skeleton.php
@@ -18,6 +18,18 @@
 		<meta name="viewport" content="width=device-width" />
 		<meta name="viewport" content="initial-scale=1.0"/>
 		<link rel="shortcut icon" type="image/x-icon" href="style/ico/<?=$opt['style']?>.ico" />
+
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-31352460-1']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 	</head>
 	<body> 
 		<div id="main">
