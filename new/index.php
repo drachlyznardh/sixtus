1c57da8c2899b6c50b24430d0b2e4bb1b31d1934
DrachLyznardh <drachlyznardh@roundhousecode.com>
2012-06-15 14:18:11 +0200

    Lol

--- new/index.php
+++ new/index.php
@@ -1,6 +1,7 @@
 title#Novità#Le dieci notizie più recenti
 next#News/Archivio/#Archivio
 start#page
+	include#new/2012/06#15
 	include#new/2012/06#14
 	include#new/2012/06#12
 	include#new/2012/06#10
@@ -11,13 +12,9 @@ start#page
 	include#new/2012/05#24
 	include#new/2012/05#23
 	include#new/2012/05#21
-	sbr#
-	include#new/2012/04#30
 stop#page
 start#side
 	include#new/2012/06#side@as@content
 	sbr#
 	include#new/2012/05#side@as@content
-	sbr#
-	include#new/2012/04#side@as@content
 stop#side
