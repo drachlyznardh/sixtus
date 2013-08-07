8b83cc1f8e5089ec2746867af37552d9b5de2bc4
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-08-07 18:04:22 +0200

    Adding stuff.

--- sys/section.php
+++ sys/section.php
@@ -31,11 +31,26 @@
 					$this->switchContext('r');
 					$this->content[] = $cmd_args[1];
 					break;
+				case 'tid':
+					$this->content[] = "<?=make_tid(\$attr, \$request['tab'], \$search['page'][0], '$cmd_args[1]', '$cmd_args[2]')?>";
+					break;
+				case 'link':
+					$dest = polish_line($cmd_args[1]);
+					$title = polish_line($cmd_args[2]);
+					$url = "<?=make_canonical(\$attr, '$dest', '$title')?>";
+					$this->content[] = "<a href=\"$url\">$title</a>";
+					break;
 				case 'title':
 					$this->closeContext();
 					if ($cmd_attr and $cmd_attr[1] == 'right')
 						$this->content[] = '<h2 class="reverse">'.$cmd_args[1].'</h2>';
 					else $this->content[] = '<h2>'.$cmd_args[1].'</h2>';
+					break;
+				case 'stitle':
+					$this->closeContext();
+					if ($cmd_attr and $cmd_attr[1] == 'right')
+						$this->content[] = '<h3 class="reverse">'.$cmd_args[1].'</h3>';
+					else $this->content[] = '<h3>'.$cmd_args[1].'</h3>';
 					break;
 				default:
 					$this->closeContext();
