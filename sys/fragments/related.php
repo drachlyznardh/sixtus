8b9c0bce97cd0cf3d6aeea4da1f3b2cabf764a2e
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-08-08 16:57:29 +0200

    before-title-after also in related pages.

--- sys/fragments/related.php
+++ sys/fragments/related.php
@@ -6,9 +6,21 @@
 <!-- Sys/Fragments/Related [Start] -->
 <div>
 <div class="section">
-	<?php if(isset($related['prev'])) { ?>
+	<?php if(isset($related['prev'])) {
+			if (preg_match('/@/', $related['prev'][1])) {
+				$_ = preg_split('/@/', $related['prev'][1]);
+				switch(count($_))
+				{
+					case 2: $before = false; $title = $_[0]; $after = $_[1]; break;
+					case 3: $before = $_[0]; $title = $_[1]; $after = $_[2]; break;
+				}
+			} else {
+				$before = $after = false;
+				$title = $related['prev'][1];
+			}
+	?>
 		<div class="inside"><p>
-			/ <a href="<?=make_canonical($attr, $related['prev'][0])?>"><?=$related['prev'][1]?></a> / <b><em>Precendente</em></b>
+			/ <?=$before?><a href="<?=make_canonical($attr, $related['prev'][0])?>"><?=$title?></a><?=$after?> / <b><em>Precendente</em></b>
 		</p></div>
 	<?php } ?>
 	<p style="text-align: center">
@@ -17,9 +29,23 @@
 			<a href="<?=make_canonical($attr, $_[0])?>"><?=$_[1]?></a> / 
 		<?php } ?>
 	</p>
-	<?php if (isset($related['next'])) { ?>
+	<?php if (isset($related['next'])) {
+			if (preg_match('/@/', $related['next'][1])) {
+				$_ = preg_split('/@/', $related['next'][1]);
+				switch(count($_))
+				{
+					case 2: $before = false; $title = $_[0]; $after = $_[1]; break;
+					case 3: $before = $_[0]; $title = $_[1]; $after = $_[2]; break;
+				}
+			} else {
+				$before = $after = false;
+				$title = $related['next'][1];
+			}
+	?>
 		<div class="outside"><p class="reverse">
-			<b><em>Successivo</em></b> / <a href="<?=make_canonical($attr, $related['next'][0])?>"><?=$related['next'][1]?></a> /
+			<b><em>Successivo</em></b> / <?=$before?><a
+			href="<?=make_canonical($attr,
+			$related['next'][0])?>"><?=$title?></a><?=$after?> /
 		</p></div>
 	<?php } ?>	
 </div>
