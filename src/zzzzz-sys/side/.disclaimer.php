1104778d4de26a4b0fb083d0c1c704eddbaf34b2
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-08-13 17:47:55 +0200

    Old sys file are no longer useful.

--- src/zzzzz-sys/side/.disclaimer.php
+++
@@ -1,9 +0,0 @@
-<div class="section">
-	<h2>Attenzione!!!</h2>
-	<p>
-		La maggior parte di queste storie è triste, triste un sacco, triste a botta!
-	</p><p>
-		La lettura può indurre, nelle persone particolarmente sensibili,
-		casi di suicidio anche reiterati.
-	</p>
-</div>
--- src/zzzzz-sys/side/nav.php
+++
@@ -1,6 +0,0 @@
-<div class="section">
-	<h2>404 Not Found</h2>
-	<p>
-		Qui non c'è nulla d'interessante.
-	</p>
-</div>
