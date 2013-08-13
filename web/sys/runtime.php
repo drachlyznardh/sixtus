a6977134e1df52004ec23022ce7f9cc2528936e1
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-08-13 17:23:02 +0200

    Fixing related and runtime sources

--- web/sys/runtime.php
+++ web/sys/runtime.php
@@ -5,6 +5,9 @@
 	$map = array (
 		'news' => array('news'),
 		'storie' => array('str',
+			'2010' => 'str/2010',
+			'2011' => 'str/2011',
+			'2012' => 'str/2012',
 			'2013' => 'str/2013'
 		)
 	);
