8a5f973af7359f4edcb444b233e4810ddda0a3f8
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-10-07 17:28:43 +0200

    Collecting tags

--- transform/pag-to-tag.php
+++ transform/pag-to-tag.php
@@ -2,9 +2,54 @@
 
 	require_once('utils.php');
 
+	function get_filename_from_tag ($name)
+	{
+		switch (strlen($name))
+		{
+			case 1: $filename = $name[0].'/'.$name[0].'.tag'; break;
+			case 2:
+			default: $filename = $name[0].'/'.$name[0].$name[1].'/'.$name.'.tag';
+		}
+
+		return $filename;
+	}
+
+	function put_array_on_file ($filename, $array_name, $array_data)
+	{
+		$to_file = '<?php';
+		foreach(array_keys($array_data) as $key)
+		{
+			$to_file .= "\n\t";
+			$to_file .= '$'.$array_name.'[\''.$key.'\'] = '.$array_data[$key].';';
+		}
+		$to_file .= '?>';
+
+		if (!file_exists($filename)) mkdir(dirname($filename), 0777, true);
+		file_put_contents($filename, $to_file);
+	}
+
+	function put_tag_into_tag_files ($taglist, $base_dir, $canonical_name)
+	{
+		//echo ("Now adding tags from [$canonical_name] to TAG_DB\n");
+		//print_r($taglist);
+		foreach (array_keys($taglist) as $key)
+		{
+			$filename = get_filename_from_tag($key);
+			//echo ("Tag [$key] goes in file [$filename]\n");
+			
+			$includename = $base_dir.$filename;
+			if (file_exists($includename)) include ($includename);
+
+			$tag[$canonical_name] = true;
+			put_array_on_file($includename, 'tag', $tag);
+			//echo ("\nWrote on [$includename]\n");
+		}
+		//echo ("Done.\n");
+	}
+
 	$taglist = array();
-	$rows = make_lines_from_file($argv[2]);
-	$base = dirname($argv[2]);
+	$rows = make_lines_from_file($argv[3]);
+	$base = dirname($argv[3]);
 
 	foreach ($rows as $_)
 	{
@@ -16,9 +61,9 @@
 		}
 	}
 
-	$page_reverse_map = make_page_reverse_map ($argv[4]);
-	$current_page = str_replace($argv[1], '', $argv[2]);
-	$current_page = str_replace('.lyz', '', $current_page);
+	$page_reverse_map = make_page_reverse_map ($argv[5]);
+	$current_file = str_replace($argv[1], '', $argv[3]);
+	$current_page = str_replace('.lyz', '', $current_file);
 	$current_page = str_replace('.pag', '', $current_page);
 	$current_page = str_replace('/index', '', $current_page);
 	//echo ("Current [$current_page]");
@@ -42,6 +87,7 @@
 	$to_file .= "\n";
 	$to_file .= '?>';
 
-	file_put_contents($argv[3], $to_file);
+	file_put_contents($argv[4], $to_file);
 
+	put_tag_into_tag_files($taglist, $argv[2], $canonical_name);
 ?>
