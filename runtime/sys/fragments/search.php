ce966b5cc618128b4a3217fb391907358d4cdc71
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-10-07 17:30:40 +0200

    Displaying form

---
+++ runtime/sys/fragments/search.php
@@ -0,0 +1,3 @@
+<span><form class="search" method="get" style="display:inline" action="Tag/Search/">
+	<input style="background-color: #444444; display:inline" type="text" value="FUCK" />
+</form></span>
