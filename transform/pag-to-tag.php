b2eb826d4fc5527a7fa4a6a8df6a256475a9a632
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-10-07 13:51:38 +0200

    Tag files get done

--- transform/pag-to-tag.php
+++ transform/pag-to-tag.php
@@ -3,41 +3,45 @@
 	require_once('utils.php');
 
 	$taglist = array();
-	$rows = make_lines_from_file($argv[1]);
-	$base = dirname($argv[1]);
-
-	print_r($argv);
+	$rows = make_lines_from_file($argv[2]);
+	$base = dirname($argv[2]);
 
 	foreach ($rows as $_)
 	{
 		if (preg_match('/tag#/', $_))
 		{
 			$result = split('#', $_);
-			print_r($result);
 			array_shift($result);
-			print_r($result);
 			foreach ($result as $r) $taglist[$r] = true;
 		}
 	}
 
-	$page_reverse_map = make_page_reverse_map ($argv[3]);
-	echo ("\n[Page Reverse Map]\n");
-	print_r($page_reverse_map);
-	echo ("[Page Reverse Map]\n\n");
-
-	print_r($taglist);
+	$page_reverse_map = make_page_reverse_map ($argv[4]);
+	$current_page = str_replace($argv[1], '', $argv[2]);
+	$current_page = str_replace('.lyz', '', $current_page);
+	$current_page = str_replace('.pag', '', $current_page);
+	$current_page = str_replace('/index', '', $current_page);
+	//echo ("Current [$current_page]");
+	if (isset($page_reverse_map[$current_page])) $canonical_name = $page_reverse_map[$current_page];
+	else {
+		$index = strrpos($current_page, '/');
+		$current_category = substr($current_page, 0, $index);
+		$current_name = substr($current_page, $index + 1);
+		$canonical_name = $page_reverse_map[$current_category].strtoupper($current_name).'/';
+	}
+	//echo (", Canonical [$canonical_name]\n");
 
 	$to_file = '<?php';
 	
 	foreach (array_keys($taglist) as $_)
 	{
 		$to_file .= "\n\t";
-		$to_file .= '$tag[\''.'CANONICAL_NAME'.'\'][\'page\'][\''.$_.'\'] = '.$taglist[$_].';';
+		$to_file .= '$tag[\''.$canonical_name.'\'][\'page\'][\''.$_.'\'] = '.$taglist[$_].';';
 	}
 	
 	$to_file .= "\n";
 	$to_file .= '?>';
 
-	file_put_contents($argv[2], $to_file);
+	file_put_contents($argv[3], $to_file);
 
 ?>
