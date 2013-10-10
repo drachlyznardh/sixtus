be717e8c8fc2d4313848577a79e569f1de3f429e
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-10-10 22:24:15 +0200

    Quiter map generation

--- transform/map-to-rmap.php
+++ transform/map-to-rmap.php
@@ -2,22 +2,12 @@
 
 	require_once('utils.php');
 
-	echo ("-- map-to-rmap.php --\n");
-	echo ("\nArgv");
-	print_r($argv);
-
 	$rmap = make_page_reverse_map ($argv[1]);
-	echo ("\nRMap");
-	print_r($rmap);
-
-	$to_file = '<?php';
+	$to_file = '<'.'?php';
 	foreach (array_keys($rmap) as $_)
 		$to_file .= "\n\t\$rmap['$_'] = '$rmap[$_]';";
-	$to_file .= "\n".'?>';
-
-	echo ($to_file);
+	$to_file .= "\n".'?'.'>';
 	file_put_contents($argv[2], $to_file);
-
-	return;
+	die();
 
 ?>
