2dc981b2dfa0e04c1a5718fc0acf5465f11bd6e7
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-10-11 13:51:01 +0200

    Cleaner

--- transform/tch-to-cloud.php
+++ transform/tch-to-cloud.php
@@ -1,18 +1,16 @@
 <?php
-	print_r($argv);
 	include($argv[1]);
 	include($argv[2]);
-	print_r($tag);
+	
 	$key = array_keys($tag)[0];
 	$update = count($tag[$key]);
-	echo ("Updated count of [$key] is [$update]");
-	$total_values[$key] = $update;
+	$cloud[$key] = $update;
 
 	$to_file = '<'.'?php';
-	foreach(array_keys($total_values) as $_)
-		$to_file .= "\n\t\$total_values['$_'] = $total_values[$_];";
+	foreach(array_keys($cloud) as $_)
+		$to_file .= "\n\t\$cloud['$_'] = $cloud[$_];";
 	$to_file .= "\n".'?'.'>';
-	echo ($to_file);
+	
 	file_put_contents($argv[2], $to_file);
 	die();
 ?>
