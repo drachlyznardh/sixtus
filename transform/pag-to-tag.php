5b19680f5e02f8c37c586c8271a3cb02e4b4bada
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-10-13 18:16:46 +0200

    Mooooore cleaning

--- transform/pag-to-tag.php
+++ transform/pag-to-tag.php
@@ -16,8 +16,6 @@
 
 	function put_tag_into_tagfile ($tagname, $basedir, $data)
 	{
-		echo("\nPut Tag Into Tagfile ($tagname, $basedir)\n");
-
 		$filename = $basedir.get_filename_from_tag($tagname);
 		$formerlist = get_array_from_tagfile($filename);
 		$newlist = $formerlist;
@@ -32,7 +30,7 @@
 			foreach(array_keys($newlist[$_]) as $__)
 				$to_file .= "\t\$tag['$_']['$__'] = '".$newlist[$_][$__]."';\n";
 		$to_file .= '?'.'>';
-		echo ($to_file);
+
 		if (!file_exists(dirname($filename))) mkdir(dirname($filename), 0777, true);
 		file_put_contents($filename, $to_file);
 		return;
@@ -41,12 +39,8 @@
 		{
 			if (strcmp($_, 'page') == 0) $key = $pagename;
 			else $key = substr($pagename, 0, -1).'§'.$_.'/';
-			#echo ("\n\t\$tag['$pagename']['$_'] = '$pagetitle[$_]';");
 			$newlist[$pagename][$_] = $pagetitle[$_];
 		}
-
-		#echo("\n -- New List\n");
-		#print_r($newlist);
 
 		$to_file = '<'.'?php';
 		foreach (array_keys($newlist) as $_)
@@ -55,12 +49,8 @@
 		$to_file .= "\n".'?'.'>';
 
 		if (!file_exists(dirname($filename))) mkdir(dirname($filename), 0777, true);
-		echo ($to_file);
-		echo ("\n\n To $filename");
 		file_put_contents($filename, $to_file);
 	}
-
-	print_r($argv);
 
 	$taglist = array();
 	$rows = make_lines_from_file($argv[3]);
@@ -96,7 +86,7 @@
 	$current_page = str_replace('.lyz', '', $current_file);
 	$current_page = str_replace('.pag', '', $current_page);
 	$current_page = str_replace('/index', '', $current_page);
-	#echo ("Current [$current_page]");
+
 	if (isset($rmap[$current_page])) $canonical_name = $rmap[$current_page];
 	else {
 		$index = strrpos($current_page, '/');
@@ -104,7 +94,6 @@
 		$current_name = substr($current_page, $index + 1);
 		$canonical_name = $rmap[$current_category].strtoupper($current_name).'/';
 	}
-	#echo (", Canonical [$canonical_name]\n");
 
 	$to_file = '<'.'?php';
 	foreach (array_keys($pagetitle) as $_)
@@ -113,27 +102,18 @@
 		foreach (array_keys($taglist[$_]) as $__)
 	{
 		$to_file .= "\n\t";
-		$to_file .=
-		'$tag[\''.$canonical_name.'\'][\''.$_.'\'][\''.ucwords($__).'\'] = '.$taglist[$_][$__].';';
+		$to_file .= '$tag[\''.$canonical_name.'\'][\''.$_.'\'][\''.ucwords($__).'\'] = '.$taglist[$_][$__].';';
 	}
 	$to_file .= "\n".'?'.'>';
-
-	echo ($to_file);
 	file_put_contents($argv[4], $to_file);
 
-	echo ("Now reversing…\n");
+	// Creating the reverse map
 	foreach (array_keys($taglist) as $_)
 		foreach (array_keys($taglist[$_]) as $__)
-		{
-			$current_tag = $__;
-			$current_tab = $_;
-			$current_title = $pagetitle[$_];
-			$rtmap[$current_tag][$current_tab][] = array($canonical_name, $current_title);
-		}
-	print_r($rtmap);
-	echo ("Done.\n");
+			$rtmap[$__][$_][] = array($canonical_name, $pagetitle[$_]);
 
 	foreach (array_keys($rtmap) as $_)
 		put_tag_into_tagfile ($_, $argv[2], $rtmap[$_]);
+
 	die();
 ?>
