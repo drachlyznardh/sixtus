191966d5fd33d9c88ccd630406f0efd39a9b5e60
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-08 15:08:14 +0100

    Reversing map.

--- transform/maps/dmap-to-rmap.php
+++ transform/maps/dmap-to-rmap.php
@@ -1,12 +1,28 @@
 <?php
 
-	require_once('utils.php');
+	function load_content($filename)
+	{
+		require_once($filename);
 
-	$rmap = make_page_reverse_map ($argv[1]);
-	$to_file = '<'.'?php';
-	foreach (array_keys($rmap) as $_)
-		$to_file .= "\n\t\$rmap['$_'] = '$rmap[$_]';";
-	$to_file .= "\n".'?'.'>';
+		if (!isset($direct)) die("\$Direct is not defined.\n");
+
+		return $direct;
+	}
+
+	$data = load_content($argv[1]);
+
+	$result = array();
+	foreach (array_keys($data) as $key)
+		if (!isset($result[$data[$key]]))
+			$result[$data[$key]] = $key;
+	ksort($result);
+
+	$to_file[] = sprintf("%s%s\n\n", '<', '?php');
+	foreach (array_keys($result) as $key)
+			$to_file[] = sprintf("\t%s['%s'] = '%s';\n",
+				'$reverse', $key, $result[$key]);
+	$to_file[] = sprintf("\n%s%s\n", '?', '>');
+
 	file_put_contents($argv[2], $to_file);
 	die();
 
