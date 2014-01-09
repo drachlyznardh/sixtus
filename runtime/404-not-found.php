e47a00bba4799d2fbd024782032a6714bdcc8435
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-09 18:44:31 +0100

    And more…

--- runtime/404-not-found.php
+++ runtime/404-not-found.php
@@ -3,7 +3,7 @@
 	$attr['title'] = '404 Not Found';
 	$attr['subtitle'] = 'No such file or directory';
 
-	require_once('sys/fragments/in-before.php');
+	require_once('page-top.php');
 ?><div class="section">
 	<p>
 		La richiesta [<code><?=$request['original']?></code>] non ha prodotto
@@ -11,9 +11,9 @@
 		stato scritto male o che io mi sia sbagliato.
 	</p>
 </div><?php
-	require_once('sys/fragments/in-middle.php');
+	require_once('page-middle.php');
 ?><div class="section">
 	<p>404 Not Found</p>
 </div><?php
-	require_once('sys/fragments/in-after.php');
+	require_once('page-bottom.php');
 ?>
