102cfd044973163b17c171dcf9c40482abc13956
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-10 19:08:56 +0100

    Much better.

--- transform/blog/create-archive.php
+++ transform/blog/create-archive.php
@@ -5,29 +5,33 @@
 	# Load map
 	require_once($argv[2]);
 
-	$year = split('/', $argv[1]);
-	$year = $year[count($year) - 2];
-	list($prev, $next) = scan_for_years(array_keys($blog_map), $year);
 	$limit = count($blog_map);
-	$keys = array_keys($blog_map);
+	$key = array_keys($blog_map);
 
 	$to_file[] = sprintf("title#%s#%s\n",
 		'Archivio', 'Tutte le notizie di sempre');
 	$to_file[] = sprintf("start#page\n");
 	
-	$to_file[] = sprintf("\ttitle#Anno %s\n", $keys[0]);
-	$to_file[] = dump_that_year ($keys[0], $blog_map[$keys[0]]);
-	for ($i = 1; $i < $limit; $i++)
+	### body
+	for ($i = 0; $i < $limit; $i++)
 	{
-		$to_file[] = sprintf("\tsec#br\n");
-		$to_file[] = sprintf("\ttitle#Anno %s\n", $keys[$i]);
-		$to_file[] = dump_that_year ($keys[$i], $blog_map[$keys[$i]]);
+		if ($i) $to_file[] = sprintf("\tsec#\n");
+		$to_file[] = sprintf("\tid#%s\n", $key[$i]);
+		$to_file[] = sprintf("\ttitle#link#Blog/%s/#%s\n", $key[$i], $key[$i]);
+		$to_file[] = dump_that_year ($key[$i], $blog_map[$key[$i]]);
 	}
+	### body
 
 	$to_file[] = sprintf("stop#page\n");
 	$to_file[] = sprintf("start#side\n");
-	foreach(array_keys($blog_map) as $year)
-		$to_file[] = sprintf("\ttitle#Anno %s\n", $year);
+
+	### side
+	$to_file[] = sprintf("\ttitle@right#Salta all'anno\n");
+	$to_file[] = sprintf("\tp#tid#%s##%s\n", $key[0], $key[0]);
+	for ($i = 1; $i < $limit; $i++)
+		$to_file[] = sprintf("\t/\n\ttid#%s##%s\n", $key[$i], $key[$i]);
+	### side
+	
 	$to_file[] = sprintf("stop#side\n");
 
 	file_put_contents($argv[1], $to_file);
