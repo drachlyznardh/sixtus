0a4ee7e954a6c4cec4371a1c70a6b870967b7a52
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-10 22:12:52 +0100

    Automatic relations!!!

--- transform/blog/create-year.php
+++ transform/blog/create-year.php
@@ -10,6 +10,8 @@
 	list($prev, $next) = scan_for_years(array_keys($blog_map), $year);
 
 	$to_file[] = sprintf("title#Notizie %s#Tutte le notizie del %s\n", $year, $year);
+	if ($prev) $to_file[] = sprintf("prev#Blog/%s/#%s\n", $prev, $prev);
+	if ($next) $to_file[] = sprintf("next#Blog/%s/#%s\n", $next, $next);
 	$to_file[] = sprintf("start#page\n");
 	$to_file[] = sprintf("stop#page\n");
 	$to_file[] = sprintf("start#side\n");
