521a5f85745ca8fea08dd6b1457d69c693b66c4d
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-10 15:15:53 +0100

    Error with corresponding file path.

--- runtime/404-not-found.php
+++ runtime/404-not-found.php
@@ -11,7 +11,7 @@
 	require_once('page-top.php');
 ?><div class="section">
 	<p>
-		La richiesta [<code><?=$request['original']?></code>] non ha prodotto
+		La richiesta [<code><?=$request['original']?> <?=$target_file?></code>] non ha prodotto
 		alcun risultato. È possibile che l'URL contenga un errore, che link sia
 		stato scritto male o che io mi sia sbagliato.
 	</p>
