2b7349b72cece900cb3471ffdd6448e18a7d12d6
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-12 00:34:24 +0100

    This won't work.

--- transform/blog/create-news.php
+++ transform/blog/create-news.php
@@ -4,6 +4,33 @@
 
 	# Load map
 	require_once($argv[2]);
+	$reverse = array_reverse($blog_map);
+	$years = array_keys($reverse, true);
+	$key = 0;
+	$result = array();
+	$counter = 0;
+
+	print_r($reverse);
+
+	while ($counter < 11)
+	{
+		$target = $reverse[$years[$key]];
+		$i = count($target) - 1;
+
+		while ($counter < 11 && $i > -1)
+		{
+			$result[$counter][] = $years[$key];
+			$result[$counter][] = $target[$i];
+			printf("Getting %s/%s\n", $years[$key], $target[$i]);
+			
+			$i--;
+			$counter++;
+		}
+
+		$key++;
+	}
+
+	#print_r($result);
 
 	$to_file[] = sprintf("title#%s#%s\n", 'Novità', 'Le notizie più recenti');
 	$to_file[] = sprintf("next#%s#%s\n", 'Blog/ARCHIVIO/', 'Archivio');
