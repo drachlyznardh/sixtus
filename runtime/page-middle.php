0504262589f34e75f90d84c9080b253bd608849b
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-11 16:03:16 +0100

    Simplifying foot.

--- runtime/page-middle.php
+++ runtime/page-middle.php
@@ -1,9 +1,7 @@
 				</div> <!-- Core -->
 				<div id="foot">
-					<!-- Require_once("Sys/Fragment/Foot") -->
-					<?php require_once("page-foot.php"); ?>
+<?php require_once('page-foot.php'); ?>
 				</div> <!-- Foot -->
 			</div>
 			<div id="right-side">
-				<!-- Require_once("Sys/Fragment/Related") -->
-				<?php require_once("page-related.php"); ?>
+<?php require_once('page-related.php'); ?>
