afd703b215cf567ce0484c238aa08aa67bb5289b
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-16 21:10:50 +0100

    Display heading.

--- runtime/page-head.php
+++ runtime/page-head.php
@@ -1,12 +1,12 @@
 <div class="section">
 <?php
-	function heading_server ()
+	function display_heading_server ()
 	{
 		foreach (explode('.', $_SERVER['HTTP_HOST']) as $_)
 			printf('[%s]', $_);
 	}
 
-	function heading_path ($attr, $request, $map)
+	function heading_path ($attr, $request, $part, $map)
 	{
 		$previous = false;
 		$address = false;
@@ -22,38 +22,79 @@
 			if (isset($map[$tmp]))
 			{
 				$missing = false;
-				printf(' / <a href="%s">%s</a>',
-					make_canonical($attr, $address, false, false), $title);
+				$canon = make_canonical($attr, $address, false, false);
+				$result['cat'][$i] = array($title, $canon);
+				#printf(' / <a href="%s">%s</a>', $canon, $title);
 			}
 			else for ($j = $i + 1; $j < $limit; $j++)
 			{
 				$tmp .= ucwords($request[$j]).'/';
 				if (isset($map[$tmp])) {
 					$missing = false;
-					printf(' / <a href="%s">%s</a>',
-						make_canonical($attr, $tmp, false, false), $title);
+					$canon = make_canonical($attr, $tmp, false, false);
+					$result['cat'][$i] = array($title, $canon);
+					#printf(' / <a href="%s">%s</a>', $result['cat'][$i][1], $title);
 					break;
 				}
 			}
 
-			if ($missing) printf(' / <a href="%s">%s</a>',
-				make_canonical($attr, $previous.strtoupper($title).'/', false, false), strtoupper($title));
+			if ($missing) {
+				$TITLE = strtoupper($title);
+				$previous .= $TITLE.'/';
+				$canon = make_canonical($attr, $previous, false, false);
+				$result['page'] = array($TITLE, $canon);
+				#printf(' / <a href="%s">%s</a>', $TITLE, $canon);
+			} else $result['page'] = false;
 		}
+
+		if ($part) {
+			$PART = strtoupper($part);
+			$canon = make_canonical($attr, $previous, $PART, false);
+			$result['part'] = array($PART, $canon);
+		} else $result['part'] = false;
+
+		return $result;
 	}
 
-	function heading_part ($attr, $address, $part)
+	function heading_part ($attr, $request, $part, $map)
 	{
-		if ($part)
-			printf(' § <a href="%s§%s">%s</a>',
-				make_canonical($attr, implode('/', $address).'/'), strtoupper($part), strtoupper($part));
+		if (!$part) return;
+
+		$previous = false;
+		$current = false;
+		$limit = count($request);
+		for ($i = 0; $i < $limit; $i++)
+		{
+			$previous = $current;
+			$current .= ucwords($request[$i]);
+		}
+
+			printf(' § <a href="%s">%s</a>',
+				make_canonical($attr, implode('/', $request).'/', $part, false), strtoupper($part));
 	}
+
+	function display_heading_path ($heading)
+	{
+		foreach ($heading['cat'] as $_)
+			printf(' / <a href="%s">%s</a>', $_[1], $_[0]);
+	}
+
+	function display_heading_page ($heading)
+	{
+		if ($heading['page'])
+			printf(' / <a href="%s">%s</a>', $heading['page'][1], $heading['page'][0]);
+		if ($heading['part'])
+			printf(' § <a href="%s">%s</a>', $heading['part'][1], $heading['part'][0]);
+	}
+	
+	$heading = heading_path($attr, $request['path'], $attr['part'], $direct);
 ?>
 	<div class="inside em">
 		<p style="text-align:center"><?php
-	heading_server();
+	display_heading_server();
 		?><br class="small-screen"/><?php
-	heading_path($attr, $request['path'], $direct);
-	heading_part($attr, $request['path'], $attr['part']);
+	display_heading_path($heading);
+	display_heading_page($heading);
 		?> / </p>
 	</div>
 	<div class="inside em"><p style="text-align:center">
