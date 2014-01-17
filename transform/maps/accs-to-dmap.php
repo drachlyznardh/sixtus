90be12010a9dcb1b467b042bfbad235106b54ad3
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-17 15:51:03 +0100

    Much better map.

--- transform/maps/accs-to-dmap.php
+++ transform/maps/accs-to-dmap.php
@@ -9,44 +9,30 @@
 		return $map;
 	}
 
+	function dump_content_help($prefix, $key, $map)
+	{
+		if ($key) $prefix .= ucwords($key).'/';	
+		if (is_array($map)) foreach (array_keys($map) as $_)
+			if ($map[$_]) $output[] = dump_content_help($prefix, $_, $map[$_]);
+			else $output[] = sprintf("\t#%s['%s'] = '%s';\n", '$direct', $prefix, $map[$_]);
+		else $output[] = sprintf("\t%s['%s'] = '%s';\n", '$direct', $prefix, $map);
+
+		return implode($output);
+	}
+
+	function dump_content($map)
+	{
+		foreach(array_keys($map) as $key)
+			$output[] = dump_content_help(ucwords($key).'/', false, $map[$key]);
+		
+		return implode($output);
+	}
+
 	$data = load_content($argv[1]);
 	ksort($data);
 
 	$to_file[] = sprintf("%s%s\n\n", '<', '?php');
-
-	foreach (array_keys($data) as $key)
-	{
-		$KEY = ucwords($key);
-		if (is_array($data[$key]))
-		{
-			foreach (array_keys($data[$key]) as $kkey)
-			{
-				$KKEY = ucwords($kkey);
-				if (is_array($data[$key][$kkey]))
-				{
-					foreach (array_keys($data[$key][$kkey]) as $kkkey)
-					{
-						if ($data[$key][$kkey][$kkkey])
-							if ($kkkey) $to_file[] = sprintf("\t%s['%s/%s/%s/'] = '%s';\n",
-								'$direct', $KEY, $KKEY, ucwords($kkkey), $data[$key][$kkey][$kkkey]);
-							else $to_file[] = sprintf("\t%s['%s/%s/'] = '%s';\n",
-								'$direct', $KEY, $KKEY, $data[$key][$kkey][$kkkey]);
-					}
-				}
-				else
-				{
-					if ($data[$key][$kkey])
-						if ($kkey) $to_file[] = sprintf("\t%s['%s/%s/'] = '%s';\n",
-							'$direct', $KEY, $KKEY, $data[$key][$kkey]);
-						else $to_file[] = sprintf("\t%s['%s/'] = '%s';\n",
-							'$direct', $KEY, $data[$key][$kkey]);
-				}
-			}
-		}
-		else $to_file[] = sprintf("\t%s['%s/'] = '%s';\n",
-			'$direct', $KEY, $data[$key]);
-	}
-
+	$to_file[] = dump_content($data);
 	$to_file[] = sprintf("\n%s%s\n", '?', '>');
 
 	file_put_contents($argv[2], $to_file);
