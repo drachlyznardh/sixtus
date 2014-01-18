ab0c1b53751b2383ec5faafec1e34767cfe779d4
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-18 17:45:40 +0100

    Cleanup.

--- transform/lyz-to-dep.php
+++ transform/lyz-to-dep.php
@@ -7,16 +7,12 @@
 	$base = dirname($argv[1]);
 
 	foreach ($rows as $_)
-	{
 		if (preg_match('/include#/', $_))
 		{
 			$request = split('#', $_);
-			#$request = split('@', $request[1]);
 			$include = make_include_filename ($base, $request[1]);
-			#printf("[%s] -> (%s)[%s] from [%s]\n", $request[1], $base, $include, $_);
 			$deps[] = $include;
 		}
-	}
 
 	file_put_contents($argv[3], sprintf('%s: %s', $argv[2], implode(' ', $deps)));
 	die();
