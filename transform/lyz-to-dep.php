5606ed995a5d5caf017dbc325140a28d4017af5c
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-18 17:25:33 +0100

    New dependecy research.

--- transform/lyz-to-dep.php
+++ transform/lyz-to-dep.php
@@ -8,18 +8,16 @@
 
 	foreach ($rows as $_)
 	{
-		if (preg_match('/include@static#/', $_))
+		if (preg_match('/include#/', $_))
 		{
 			$request = split('#', $_);
-			$request = split('@', $request[1]);
-			$include = make_include_filename ($base, $request[0]);
-			#printf("[%s] -> [%s] from [%s]\n", $request[0], $include, $_);
+			#$request = split('@', $request[1]);
+			$include = make_include_filename ($base, $request[1]);
+			printf("[%s] -> (%s)[%s] from [%s]\n", $request[1], $base, $include, $_);
 			$deps[] = $include;
 		}
 	}
 
-	echo $argv[2];
-	echo ':';
-	foreach ($deps as $_) echo ' '.$_;
-	printf("\n");
+	file_put_contents($argv[3], printf('%s: %s', $argv[2], implode(' ', $deps)));
+	die();
 ?>
