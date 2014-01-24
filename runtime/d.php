26d8b5a7b5dd5242aaaa104ac4244b7c484e8fa8
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-01-24 15:19:14 +0100

    Also switching order, required for some reason.

--- runtime/d.php
+++ runtime/d.php
@@ -9,7 +9,7 @@
 	require_once('mimes.php');
 	require_once('direct-map.php');
 
-	$request['original'] = urldecode(mb_strtolower($_SERVER['REQUEST_URI'], 'UTF-8'));
+	$request['original'] = mb_strtolower(urldecode($_SERVER['REQUEST_URI']), 'UTF-8');
 	
 	$attr['included'] = false;
 	$attr['sections'] = true;
