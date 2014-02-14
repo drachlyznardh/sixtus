ae0382b2cdf8a20dd023755233e5f5f317fb93ae
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-14 18:39:08 +0100

    Splitting content in environmnet.

--- transform/pag-to-frag.php
+++ transform/pag-to-frag.php
@@ -2,6 +2,12 @@
 
 	var_dump($argv);
 
+	$meta = array();
+	$body = array();
+	$side = array();
+
+	$state = 0;
+	$current = &$meta;
 	$srcfile = $argv[1];
 	$row = file($srcfile);
 	
@@ -10,6 +16,28 @@
 		$line = trim($row[$lineno]);
 
 		printf("%10s %04d: [%s]\n", $srcfile, $lineno, $line);
+		
+		if (preg_match('/#/', $line))
+		{
+			$token = split('#', $line);
+
+			switch ($token[0]) {
+				case 'start':
+					printf("\tEntering [%s]\n", $token[1]);
+					$current = &$$token[1];
+					break;
+				case 'stop':
+					printf("\tLeaving [%s]\n", $token[1]);
+					break;
+				default:
+					$current[] = array($srcfile, $lineno, $line);
+			}
+		}
+		else $current[] = array($srcfile, $lineno, $line);
 	}
+
+	print_r($meta);
+	print_r($body);
+	print_r($side);
 
 ?>
