24dcee814c28fc6469270ff593e4d930841f3f7b
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-14 18:38:54 +0100

    Reading content.

--- transform/pag-to-frag.php
+++ transform/pag-to-frag.php
@@ -2,4 +2,14 @@
 
 	var_dump($argv);
 
+	$srcfile = $argv[1];
+	$row = file($srcfile);
+	
+	foreach (array_keys($row) as $lineno)
+	{
+		$line = trim($row[$lineno]);
+
+		printf("%10s %04d: [%s]\n", $srcfile, $lineno, $line);
+	}
+
 ?>
