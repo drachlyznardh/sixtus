4a505a89c723aafd8057758051d9bedf3db78b2a
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-15 15:44:43 +0100

    Refactoring.

--- transform/pag-to-frag.php
+++ transform/pag-to-frag.php
@@ -15,53 +15,83 @@
 		foreach($content as $_) printf("%d %04d %s\n", 0, $_[1], $_[2]);
 	}
 
-	$meta  = array();
-	$body  = array();
-	$ghost = array();
-	$side  = array();
+	class Splitter {
 
-	$statename = array('meta', 'body', 'ghost', 'side');
-	$state = 'meta';
-	$current = &$meta;
-	$srcfile = $argv[1];
-	$row = file($srcfile);
-	
-	foreach (array_keys($row) as $lineno)
-	{
-		$line = trim($row[$lineno]);
+		private $meta;
+		private $body;
+		private $ghost;
+		private $side;
 
-		if (preg_match('/#/', $line))
+		private $state;
+		private $sourcefile;
+
+		public __construct ()
 		{
-			$token = split('#', $line);
-
-			switch ($token[0]) {
-				case 'stop':
-					break;
-				case 'start':
-					if ($token[1] != 'meta' && $token[1] != 'body'
-						&& $token[1] != 'ghost' && $token[1] != 'side')
-							fail('Unkown environment '.$token[1], $srcfile, $lineno);
-					$state = $token[1];
-					$current = &$$token[1];
-					break;
-				case 'tab':
-					if ($state != 'body' && $state != 'ghost')
-						fail('No tabs allowed in '.$state, $srcfile, $lineno);
-					${$state}[$token[1]] = array();
-					$current = &${$state}[$token[1]];
-					break;
-				default:
-					$current[] = array(&$srcfile, $lineno, $line);
-			}
+			$this->meta  = array();
+			$this->body  = array();
+			$this->ghost = array();
+			$this->side  = array();
 		}
-		else $current[] = array(&$srcfile, $lineno, $line);
+
+		public function parse ($target)
+		{
+
+			$this->state = 'meta';
+			$current = &$meta;
+			$sourcefile = $target;
+
+			$row = file($sourcefile);
+			
+			foreach (array_keys($row) as $lineno)
+			{
+				$line = trim($row[$lineno]);
+
+				if (preg_match('/#/', $line))
+				{
+					$token = split('#', $line);
+
+					switch ($token[0]) {
+						case 'stop':
+							break;
+						case 'start':
+							if ($token[1] != 'meta' && $token[1] != 'body'
+								&& $token[1] != 'ghost' && $token[1] != 'side')
+									fail('Unkown environment '.$token[1], $srcfile, $lineno);
+							$state = $token[1];
+							$current = &$$token[1];
+							break;
+						case 'tab':
+							if ($state != 'body' && $state != 'ghost')
+								fail('No tabs allowed in '.$state, $srcfile, $lineno);
+							${$state}[$token[1]] = array();
+							$current = &${$state}[$token[1]];
+							break;
+						default:
+							$current[] = array(&$srcfile, $lineno, $line);
+					}
+				}
+				else $current[] = array(&$srcfile, $lineno, $line);
+			}
+
+		}
+		
+		public function dump () {
+
+			output_on_file('meta', array($srcfile), $meta);
+			foreach(array_keys($body) as $_)
+				output_on_file('tab-'.$_, array($srcfile), $body[$_]);
+			foreach(array_keys($ghost) as $_)
+				output_on_file('ghost-'.$_, array($srcfile), $ghost[$_]);
+			output_on_file('side', array($srcfile), $side);
+		}
+
+		public function split ($target)
+		{
+			$this->parse($target);
+			$this->dump();
+		}
 	}
 
-	output_on_file('meta', array($srcfile), $meta);
-	foreach(array_keys($body) as $_)
-		output_on_file('tab-'.$_, array($srcfile), $body[$_]);
-	foreach(array_keys($ghost) as $_)
-		output_on_file('ghost-'.$_, array($srcfile), $ghost[$_]);
-	output_on_file('side', array($srcfile), $side);
+	new Parser().split($argv[1]);
 
 ?>
