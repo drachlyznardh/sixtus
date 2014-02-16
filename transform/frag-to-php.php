ee168e0253bdfb1c2bf55bcec3ffd326e4aac081
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-16 14:00:13 +0100

    Checking files and line format.

--- transform/frag-to-php.php
+++ transform/frag-to-php.php
@@ -28,9 +28,27 @@
 	if (!preg_match($pattern, $row[0]))
 		fail("Wrong format", $argv[1], 1);
 
-	foreach ($row as $_)
-		printf("%s\n", $_);
+	$filetype = preg_replace($pattern, '$1', $row[0]);
+	$i = 1;
+	array_shift($row);
+	$filenames = array();
+	$pattern ='/^[0-9]+ (.+)$/'; 
+	while (true)
+		if (preg_match($pattern, $row[0]))
+		{
+			$filenames[] = preg_replace($pattern, '$1', $row[0]);
+			array_shift($row);
+			$i++;
+		}
+		else break;
+	
+	if (!preg_match('/^####$/', $row[0]))
+		fail("Missing separator", $argv[1], $i);
+	
+	array_shift($row);
+	$pattern ='/^([0-9]+) ([0-9]+) (.*)$/'; 
+	foreach ($filenames as $_)
+		printf("\t[%s]\n", $_);
 
 	exit(1);
-
 ?>
