f93d7268c129474adb2e2392830c3670e6589138
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-16 15:12:01 +0100

    Getting ready for content.

--- transform/frag/content-to-php.php
+++ transform/frag/content-to-php.php
@@ -1,4 +1,25 @@
 <?php
-	printf("Not yet implemented\n");
-	exit(1);
+	class Parser {
+		
+		public function parse ($f, $l, $s)
+		{}
+
+		public function dump ($target)
+		{
+			$out = array();
+
+			file_put_contents($target, $out);
+		}
+	}
+
+	$p = new Parser();
+	foreach ($row as $_)
+	{
+		list($f, $l, $s) = check_line_format($_, $i++);
+		$p->parse($f, $l, $s);
+	}
+
+	$p->dump($argv[2]);
+
+	exit(0);
 ?>
