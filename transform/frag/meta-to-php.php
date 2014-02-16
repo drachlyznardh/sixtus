06c4c8059e28ad0133bd615b1c1d93f46db7f9d5
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-16 14:21:34 +0100

    Getting ready…

--- transform/frag/meta-to-php.php
+++ transform/frag/meta-to-php.php
@@ -1,6 +1,44 @@
 <?php
 
-	printf("\nNow parsing META\n");
-	
+	class Parser {
+		
+		private $title = false;
+		private $short = false;
+		private $subtitle = false;
+
+		private $prev = false;
+		private $next = false;
+
+		public function parse ($f, $l, $s)
+		{
+			if (!preg_match('/#/', $s))
+				fail ("No meta command in [$s]", $f, $l);
+
+			$token = split('#', $s);
+			switch($token[0]) {
+				case '': break;
+				case 'tag': break;
+				case 'title':
+			}
+		}
+
+		public function dump ()
+		{
+			$out[] = sprintf("%s?php\n", '<');
+			$out[] = sprintf("?%s\n", '>');
+			
+			printf("%s", implode($out));
+		}
+	}
+
+	$p = new Parser();
+	foreach ($row as $_)
+	{
+		list($f, $l, $s) = check_line_format ($_, $i++);
+		$p->parse($f, $l, $s);
+	}
+
+	$p->dump();
+
 	exit(0);
 ?>
