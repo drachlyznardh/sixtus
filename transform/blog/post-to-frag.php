b6e9ac5e6fe8be11238a7b1b4bb7dd8ebfd25b6a
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-18 21:43:29 +0100

    Ouputting meta file.

--- transform/blog/post-to-frag.php
+++ transform/blog/post-to-frag.php
@@ -150,9 +150,10 @@
 	$to_file[] = sprintf("tabs#all_or_one\n");
 	$to_file[] = sprintf("start#page\n");
 
-	print_r($argv);
-	dump_meta($argv[1], sprintf('%smeta.php', $argv[4]), name_that_month($month), $year, scan_for_month($month, $year, $blog_map));
-	exit(1);
+	dump_meta($argv[1], sprintf('%smeta.frag', $argv[4]),
+	name_that_month($month), $year, scan_for_month($month, $year, $blog_map),
+	array_keys($out_rows));
+	exit(0);
 
 	foreach (array_keys($out_rows) as $day)
 	{
