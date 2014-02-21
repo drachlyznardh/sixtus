43d024153f8e7863df9a50fd98453e7e15d62390
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-21 19:25:51 +0100

    Looking nice.

--- runtime/page/style.php
+++ runtime/page/style.php
@@ -22,18 +22,22 @@
 		if ($attr['layout'])
 		{
 			$attr['layout'] = false;
-			printf('<em>All-Tabs</em> | <a href="%s">One-Tab</a>',
+			printf('<em>All</em> | <a href="%s">One</a>',
 				make_canonical($attr, $attr['self'], $attr['part']));
 		} else {
 			$attr['layout'] = true;
-			printf('<a href="%s">All-Tabs</a> | <em>One-Tab</em>',
+			printf('<a href="%s">All</a> | <em>One</em>',
 				make_canonical($attr, $attr['self'], $attr['part']));
 		}
 	}
 ?>
 <div class="section">
-	<h3 class="reverse">Theme</h3>
-	<p class="reverse">[ <?php display_style_choice ($attr, $style); ?> ]</p>
-	<h3>Layout</h3>
-	<p>[ <?php display_layout_choice ($attr); ?> ]</p>
+	<div class="half-right-out">
+		<h3 class="reverse">Theme</h3>
+		<p class="reverse">[ <?php display_style_choice ($attr, $style); ?> ]</p>
+	</div><div class="half-left-out">
+		<h3>Layout</h3>
+		<p>[ <?php display_layout_choice ($attr); ?> ]</p>
+	</div><div style="clear: both">
+	</div>
 </div>
