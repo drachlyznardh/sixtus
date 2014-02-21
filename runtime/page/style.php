c33c14c363b38a02d37da325950738ebe46de8d3
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-02-21 19:20:09 +0100

    Also displaying layout choice.

--- runtime/page/style.php
+++ runtime/page/style.php
@@ -17,8 +17,23 @@
 			}
 		}
 	}
+	function display_layout_choice ($attr)
+	{
+		if ($attr['layout'])
+		{
+			$attr['layout'] = false;
+			printf('<em>All-Tabs</em> | <a href="%s">One-Tab</a>',
+				make_canonical($attr, $attr['self'], $attr['part']));
+		} else {
+			$attr['layout'] = true;
+			printf('<a href="%s">All-Tabs</a> | <em>One-Tab</em>',
+				make_canonical($attr, $attr['self'], $attr['part']));
+		}
+	}
 ?>
 <div class="section">
 	<h3 class="reverse">Theme</h3>
 	<p class="reverse">[ <?php display_style_choice ($attr, $style); ?> ]</p>
+	<h3>Layout</h3>
+	<p>[ <?php display_layout_choice ($attr); ?> ]</p>
 </div>
