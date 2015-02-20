7d622d1b2665fddf33fcf4efc6788a90bdb82bd4
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-02-20 16:31:56 +0100

    Matching

--- v10/deploy/six-to-php.py
+++ v10/deploy/six-to-php.py
@@ -2,6 +2,7 @@
 # -*- encoding: utf-8 -*-
 
 import sys
+import re
 
 print
 for i in sys.argv: print("[%s]" % i)
@@ -11,6 +12,13 @@ f = open(sys.argv[1], 'r')
 
 for i in f:
 	line = i.strip()
-	print("{%s}" % line)
+
+	if '#' not in line:
+		print("{%s}" % line)
+		continue
+
+	if re.match(r'^title#(.*)', line):
+		title = re.sub(r'^title#(.*)', r'\1', line)
+		print("Title (%s)" % title)
 
 print
