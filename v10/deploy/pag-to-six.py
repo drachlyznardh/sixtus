a4949072a2f514855941ac90c814f8ecb8ed216b
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-02-22 17:44:15 +0100

    One more argument

--- v10/deploy/pag-to-six.py
+++ v10/deploy/pag-to-six.py
@@ -5,8 +5,11 @@ from __future__ import print_function
 import sys
 from splitter import Splitter
 
-if len(sys.argv) != 6:
-	print("Usage: %s <pag file> <map file> <location> <pagename> <build dir>", file=sys.stderr)
+if len(sys.argv) != 7:
+	args = ['<pag file>', '<map file>',
+		'<page location>', '<page name>',
+		'<build dir>', '<touch file>']
+	print("Usage: %s %s" % (sys.argv[0], ' '.join(args)), file=sys.stderr)
 	sys.exit(1)
 
 Splitter().load_parameters(sys.argv[2:]).split_file(sys.argv[1]).dump_output()
