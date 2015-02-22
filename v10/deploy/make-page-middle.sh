549858822617bb1a2e8c0b0e4afdf3104c57619d
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-02-22 18:22:30 +0100

    Progress

--- v10/deploy/make-page-middle.sh
+++ v10/deploy/make-page-middle.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [ $# -ne 8 ]; then
-	echo "Usage: $0 <php.in neck file> <php.in left-side file> <php.in knee file> <php file>
-		@SITE_COPYRIGHT_YEARS@ @SITE_COPYRIGHT OWNER@
-		@SITE_KEYWORD_PREV@ @SITE_KEYWORD_NEXT@"
+	files="<php.in neck file> <php.in left-side file> <php.in knee file> <php file>"
+	params="@SITE_COPYRIGHT_YEARS@ @SITE_COPYRIGHT OWNER@ @SITE_PAGE_PREV@ @SITE_PAGE_NEXT@"
+	echo "Usage: $0 $files $params"
 	exit 1
 fi
 
-cat $1 | sed -e "s/\@SITE_COPYRIGHT_YEARS\@/${5}/" -e "s/\@SITE_COPYRIGHT_OWNER\@/${6}/" > $4
+cat $1 | sed -e "s/\@SITE_COPYRIGHT_YEARS\@/${5}/" \
+	-e "s/\@SITE_COPYRIGHT_OWNER\@/${6}/" > $4
 cat $2 >> $4
-cat $3 | sed -e "s/\@SITE_KEYWORD_PREV\@/${7}/" -e "s/\@SITE_KEYWORD_NEXT\@/${8}/" >> $4
+cat $3 | sed -e "s/\@SITE_PAGE_PREV\@/${7}/" \
+	-e "s/\@SITE_PAGE_NEXT\@/${8}/" >> $4
