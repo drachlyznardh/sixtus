3ac105f09ba96d8ba4400e8963cbe503ad114768
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-02-23 12:49:12 +0100

    It works

--- v10/deploy/splitter.py
+++ v10/deploy/splitter.py
@@ -98,7 +98,7 @@ class Splitter:
 		with open(index_path, 'w') as outfile:
 			outfile.write(filecontent)
 
-	def dump_tabs (self):
+	def output_many_tabs (self, base, page_name, build_dir):
 
 		if self.debug: print('Dump Tabs', file=sys.stderr)
 
@@ -106,7 +106,7 @@ class Splitter:
 
 			if name == None: continue
 
-			file_path = '%s%s/%s/index.six' % (sys.argv[5], self.pag_path, name.upper())
+			file_path = '%s%s%s/%s/index.six' % (build_dir, base, page_name, name.upper())
 			self.touch_files.append(file_path)
 
 			self.check_dir_path(file_path)
@@ -114,10 +114,10 @@ class Splitter:
 			varmeta = self.meta
 
 			if name in self.prevs.keys() and self.prevs[name]:
-				varmeta += '\ntabprev#/%s/%s/' % (self.pag_path, self.prevs[name].upper())
+				varmeta += '\ntabprev#/%s/%s/' % (base, self.prevs[name].upper())
 
 			if name in self.nexts.keys() and self.nexts[name]:
-				varmeta += '\ntabnext#/%s/%s/' % (self.pag_path, self.nexts[name].upper())
+				varmeta += '\ntabnext#/%s/%s/' % (base, self.nexts[name].upper())
 
 			filecontent = ('%s\nstart#side\n%s\nstart#page\n%s' % (varmeta, self.side, value))
 			with open(file_path, 'w') as outfile:
@@ -132,18 +132,6 @@ class Splitter:
 		with open(index_path, 'w') as outfile:
 			outfile.write(filecontent)
 
-	def dump_touch (self):
-
-		if self.debug: print('Dump Touch', file=sys.stderr)
-
-	def dump_output (self):
-
-		if self.first:
-			self.dump_index()
-			self.dump_tabs()
-			self.dump_touch()
-		else: self.dump_single_tab()
-
 	def output_tab_files (self, base, page_name, build_dir):
 
 		index_path = '%s%s/%s/index.six' % (build_dir, base, page_name)
@@ -151,7 +139,7 @@ class Splitter:
 
 		if self.first:
 			self.output_index_file(base, index_path)
-			self.output_many_tabs()
+			self.output_many_tabs(base, page_name, build_dir)
 		else: self.output_single_tab(index_path)
 
 	def output_touch_file (self, touch_file, origin_files):
