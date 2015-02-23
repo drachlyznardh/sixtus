3f2e3cbfa43f96c3dc53ce39cc54e822acb29d79
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-02-23 11:07:05 +0100

    We have a preprocessor

--- v10/deploy/pag-to-six.py
+++ v10/deploy/pag-to-six.py
@@ -12,4 +12,10 @@ if len(sys.argv) != 7:
 	print("Usage: %s %s" % (sys.argv[0], ' '.join(args)), file=sys.stderr)
 	sys.exit(1)
 
+pag_local_dir = '%s/' % '/'.join(sys.argv[1].split('/')[:-1])
+pp = splitter.Preprocessor(pag_local_dir)
+pp.parse_file(sys.argv[1])
+
+print('%s' % '\n'.join(pp.content), file=sys.stderr)
+
 Splitter().load_parameters(sys.argv[2:]).split_file(sys.argv[1]).dump_output()
