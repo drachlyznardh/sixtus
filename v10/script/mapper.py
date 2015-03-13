a141b31a06792bc736c38afdb33c334260801d8e
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-13 10:53:38 +0100

    Simpler

--- v10/script/mapper.py
+++ v10/script/mapper.py
@@ -5,12 +5,6 @@ from __future__ import print_function
 import sys
 import os
 
-def map_capitalize (name):
-	return name.capitalize()
-
-def map_upper (name):
-	return name.upper()
-
 def get (map_file, page_origin):
 
 	with open(map_file, 'r') as f:
@@ -21,11 +15,4 @@ def get (map_file, page_origin):
 	while partial and partial not in sitemap:
 		partial = '/'.join(partial.split('/')[:-1])
 
-	if partial in sitemap:
-		value = sitemap[partial]
-		if value[1]: func = map_capitalize
-		else: func = map_upper
-		name = '%s/%s' % (value[0], page_origin[len(partial):])
-		return (name, func)
-
-	return (page_origin, map_capitalize)
+	return sitemap.get(partial, page_origin)
