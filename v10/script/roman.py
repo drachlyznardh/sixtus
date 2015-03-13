03a5d49cee833c0248d7496da6d21f6b8f9c1534
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-13 10:58:14 +0100

    Roman conversion in a function

--- v10/script/roman.py
+++ v10/script/roman.py
@@ -4,3 +4,8 @@
 import re
 
 test = re.compile(r'''^(m{0,3})(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3})$''')
+
+def convert (name):
+
+	if test.match(test): return name.upper()
+	return name.capitalize()
