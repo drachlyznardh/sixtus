f127c12f4154d59f972652f71e4417b789b6c679
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-13 15:55:31 +0100

    Cleaning

--- v10/script/splitter.py
+++ v10/script/splitter.py
@@ -167,12 +167,10 @@ class Splitter:
 
 		elif self.first:
 
-			#path = os.path.normpath(os.path.join(build_dir, base, page_name, 'index.jump.six'))
 			path = self.get_path(build_dir, page_name, 'jump')
 			self.touch_files.append(path)
 			self.output_index_file(page_name, path)
 
-			#path = os.path.normpath(os.path.join(build_dir, base, page_name, 'index.side.six'))
 			path = self.get_path(build_dir, page_name, 'side')
 			self.touch_files.append(path)
 			self.output_side_file(path)
@@ -181,7 +179,6 @@ class Splitter:
 
 		else:
 
-			#path = os.path.normpath(os.path.join(build_dir, base, page_name, 'index.page.six'))
 			path = self.get_path(build_dir, page_name, 'page')
 			self.touch_files.append(path)
 			self.output_single_tab(path)
