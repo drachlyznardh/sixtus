1d54a32b1b99e909750774716b3235bce78dc68e
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-14 14:32:08 +0100

    Another blog link

--- v10/script/poster.py
+++ v10/script/poster.py
@@ -109,7 +109,7 @@ class Poster:
 
 		with open(filename, 'w') as f:
 			print('id#%s-%s' % (self.this_page[0], self.this_page[1]), file=f)
-			print('stitle#%s %s' % (self.this_page[2], self.this_page[0]), file=f)
+			print('stitle#link#Blog/%s/%s/#%s %s' % (self.this_page[0], self.this_page[1], self.this_page[2], self.this_page[0]), file=f)
 			for number, value in sorted(self.post_title.items()):
 				print('p#<code>%s/%s</code> – ' % (number, self.this_page[1]), file=f)
 				print('\n&amp;\n'.join(['link#%s#%s#%s/%s' % (this_url, value[i], number, i) for i in xrange(len(value))]), file=f)
