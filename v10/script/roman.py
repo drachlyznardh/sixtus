e2880c71642445fa4f5e350786cd64d751f82924
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-16 12:40:59 +0100

    Another

--- v10/script/roman.py
+++ v10/script/roman.py
@@ -4,9 +4,16 @@
 import re
 
 test = re.compile(r'''^(m{0,3})(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3})$''')
+index = re.compile(r'(.*)/Index$')
 
 def convert (name):
 
 	if test.match(name): return name.upper()
 	if name.islower(): return name.capitalize()
+	return name
+
+def clear (name):
+
+	if index.match(name):
+		return index.sub(r'\1', name)
 	return name
