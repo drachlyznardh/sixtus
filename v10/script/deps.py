3846932a7a2f2c93053a3c6ce633b4c7c3b336e4
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-17 15:03:23 +0100

    Names

--- v10/script/deps.py
+++ v10/script/deps.py
@@ -3,6 +3,8 @@
 
 from __future__ import print_function
 import re
+import os
+
 import roman
 
 def extract (filename):
@@ -22,32 +24,34 @@ def extract (filename):
 			elif re_tab.match(line):
 				tabs.append(re_tab.sub(r'\1', line))
 			elif re_jump.match(line):
-				return True, [], []
+				return True, sources, tabs
 
 	return False, roman.unique(sources), roman.unique(tabs)
 
 def insert (filename, destination, jump, sources, tabs):
 
+	d = '%s/' % os.path.normpath(destination)
+
 	with open(filename, 'w') as f:
 
 		if jump:
 
-			print('SIX_FILES += %sjump.six' % destination, file=f)
-			print('%sjump.six: %s.done' % (destination, destination), file=f)
-			print('%s.done: %s' % (destination, ' '.join(sources)), file=f)
+			print('SIX_FILES += %sjump.six' % d, file=f)
+			print('%sjump.six: %s.done' % (d, d), file=f)
+			print('%s.done: %s' % (d, ' '.join(sources)), file=f)
 			return
 
 		if len(tabs) == 0:
 
-			print('SIX_FILES += %spage.six' % destination, file=f)
-			print('%spage.six: %s.done' % (destination, destination), file=f)
-			print('%s.done: %s' % (destination, ' '.join(sources)), file=f)
+			print('SIX_FILES += %spage.six' % d, file=f)
+			print('%spage.six: %s.done' % (d, d), file=f)
+			print('%s.done: %s' % (d, ' '.join(sources)), file=f)
 			return
 
-		files = ['%s%s/page.six' % (destination, roman.convert(name)) for name in tabs ]
-		files.append('%sjump.six' % destination)
-		files.append('%sside.six' % destination)
+		files = ['%s%s/page.six' % (d, roman.convert(name)) for name in tabs ]
+		files.append('%sjump.six' % d)
+		files.append('%sside.six' % d)
 
 		print('SIX_FILES += %s' % ' '.join(files), file=f)
-		print('%s: %s.done' % (' '.join(files), destination), file=f)
-		print('%s.done: %s' % (destination, ' '.join(sources)), file=f)
+		print('%s: %s.done' % (' '.join(files), d), file=f)
+		print('%s.done: %s' % (d, ' '.join(sources)), file=f)
