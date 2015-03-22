e9b0f8ac1d7fc56a6d67f74137ea0c2bd9b89ea8
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-22 15:06:33 +0100

    Slightly more complicated.

---
+++ v10/scons/testing/hello.h
@@ -0,0 +1,6 @@
+#ifndef __HELLO_H_
+#define __HELLO_H__
+
+void hello ();
+
+#endif // __HELLO_H__
--- v10/scons/testing/main.c
+++ v10/scons/testing/main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "hello.h"
 
 int main (int argc, char **argv)
 {
-	printf("Hello World\n");
+	hello();
 	return 0;
 }
