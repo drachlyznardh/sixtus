faa6980149743762d0164242ccf2ba756c9b892d
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-22 17:13:54 +0100

    Much cleaner

--- v10/script/main.py
+++ v10/script/main.py
@@ -16,6 +16,18 @@ def find_all_dirs (source):
 
 	return found
 
+def find_all_files (root_dir, pattern):
+
+	visit = [root_dir] + find_all_dirs(root_dir)
+	all_files = []
+
+	for d in visit:
+		for f in os.listdir(d):
+			if fnmatch.fnmatch(f, pattern):
+				all_files.append(f)
+
+	return all_files
+
 print('Siχtus 0.10')
 
 import os
@@ -25,18 +37,18 @@ if not os.path.exists('src'):
 	print('Path [src] does not exist!', file=sys.stderr)
 	sys.exit(1)
 
-visit = ['src'] + find_all_dirs('src')
-print(visit)
-pag_files = []
+#visit = ['src'] + find_all_dirs('src')
+#print(visit)
+pag_files = find_all_files ('src', '*.pag')
 
-for d in visit:
-	for i in os.listdir(d):
-
-		print('Found [%s]' % i)
-
-		if fnmatch.fnmatch(i, '*.pag'):
-			print('%s matches *.pag' % i)
-			pag_files.append(i)
+#for d in visit:
+#	for i in os.listdir(d):
+#
+#		print('Found [%s]' % i)
+#
+#		if fnmatch.fnmatch(i, '*.pag'):
+#			print('%s matches *.pag' % i)
+#			pag_files.append(i)
 
 print(pag_files)
 
