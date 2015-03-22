aee723d2726b7e4aba478163e5c5c915b09dd50c
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-22 17:09:24 +0100

    Looking for *.pag files

--- v10/script/main.py
+++ v10/script/main.py
@@ -4,6 +4,18 @@
 from __future__ import print_function
 import sys
 
+def get_all_dirs (source):
+
+	found = []
+
+	for each in os.listdir(source):
+		child = os.path.join(source, each)
+		if os.path.isdir(child):
+			found.append(child)
+			found += get_all_dirs(child)
+
+	return found
+
 print('Siχtus 0.10')
 
 import os
@@ -13,15 +25,20 @@ if not os.path.exists('src'):
 	print('Path [src] does not exist!', file=sys.stderr)
 	sys.exit(1)
 
-visit = ['src']
+visit = ['src'] + get_all_dirs('src')
+print(visit)
+pag_files = []
 
-while len(visit):
-	j = visit.pop(0)
-	for i in os.listdir(j):
+for d in visit:
+	for i in os.listdir(d):
+
 		print('Found [%s]' % i)
 
 		if fnmatch.fnmatch(i, '*.pag'):
 			print('%s matches *.pag' % i)
+			pag_files.append(i)
+
+print(pag_files)
 
 print('Siχtus 0.10, done')
 sys.exit(0)
