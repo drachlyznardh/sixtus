04108f9ad6d0378e20321623bf2ea0676af47437
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-22 18:45:35 +0100

    Full list of PHP files

--- v10/script/main.py
+++ v10/script/main.py
@@ -46,7 +46,7 @@ print('Siχtus 0.10')
 
 pag_files = find_all_files ('src', '*.pag')
 Six_files = [re.sub(r'^src(.*)\.pag$', r'build\1.Six', i) for i in pag_files]
-dep_files = [re.sub(r'(.*).Six$', r'\1.dep', i) for i in Six_files]
+dep_files = [re.sub(r'(.*)\.Six$', r'\1.dep', i) for i in Six_files]
 
 print('pag_files = %s' % pag_files)
 print('Six_files = %s' % Six_files)
@@ -73,12 +73,46 @@ for dep_file in dep_files:
 	if not os.path.exists(dep_file):
 		print('dep file [%s] does not exist!' % dep_file)
 
-		Six_file = re.sub(r'(.*).dep', r'\1.Six', dep_file)
+		Six_file = re.sub(r'(.*)\.dep', r'\1.Six', dep_file)
 		print('Reading dependencies from %s' % Six_file)
 		dep_list = deps.extract(Six_file)
 		assert_dir(dep_file)
 		with open(dep_file, 'w') as f:
 			print('(%s, %s, %s)' % dep_list, file=f)
+
+import mapper
+import roman
+
+php_names = []
+
+for dep_file in dep_files:
+
+	with open(dep_file, 'r') as f:
+		dep_list = eval(f.read())
+
+	tab_names = dep_list[2]
+	tab_files = []
+	stem = re.sub(r'build/(.*)\.dep', r'\1', dep_file)
+	mapped = '%s' % mapper.get('map.py', os.path.dirname(stem))
+	if len(mapped): mapped = '%s/' % mapped
+	print('Stem %s → %s → %s' % (dep_file, stem, mapped))
+	size = len(tab_names)
+	if size == 0:
+		tab_files.append('%sindex' % mapped)
+	elif size == 1:
+		tab_files.append('%sjump' % mapped)
+		tab_files.append('%s%s' % (mapped, tab_names[0]))
+	else:
+		tab_files.append('%sjump' % mapped)
+		tab_files.append('%sside' % mapped)
+		for name in tab_names:
+			tab_files.append('%s%s/page' % (mapped, roman.convert(name)))
+
+	php_names += tab_files
+	print(tab_files)
+
+php_files = [os.path.join('/opt/web/mobile', '%s.php' % name) for name in php_names]
+print('php_files = %s' % php_files)
 
 print('Siχtus 0.10, done')
 sys.exit(0)
