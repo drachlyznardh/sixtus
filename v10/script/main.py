b23f11fb6462196dae5db983453a11f1cfca0bb9
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-22 16:53:55 +0100

    Check

--- v10/script/main.py
+++ v10/script/main.py
@@ -5,4 +5,13 @@ from __future__ import print_function
 import sys
 
 print('Siχtus 0.10')
+
+import os
+import fnmatch
+
+if not os.path.exists('src'):
+	print('Path [src] does not exist!', file=sys.stderr)
+	sys.exit(1)
+
+print('Siχtus 0.10, done')
 sys.exit(0)
