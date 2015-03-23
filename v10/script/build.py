5353df18cd42ee2223d6cce9668ebe233008f6ba
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-23 16:12:58 +0100

    Split

--- v10/script/build.py
+++ v10/script/build.py
@@ -8,6 +8,8 @@ import os
 import util
 import preprocessor
 import deps
+import mapper
+import roman
 
 def build_Six_file (Six_file):
 
@@ -28,4 +30,33 @@ def build_dep_file (dep_file):
 	util.assert_dir(dep_file)
 	with open(dep_file, 'w') as f:
 		print('(%s, %s, %s)' % dep_list, file=f)
+
+def parse_dep_file (dep_file):
+
+	with open(dep_file, 'r') as f:
+		jump, sources, tab_names = eval(f.read())
+
+	tab_files = []
+
+	stem = re.sub(r'build/(.*)\.dep', r'\1', dep_file)
+	mapped = '%s' % mapper.get('map.py', os.path.dirname(stem))
+	basename = os.path.basename(stem)
+	if basename != 'index':
+		mapped = os.path.join(mapped, roman.convert(basename))
+
+	size = len(tab_names)
+	if jump:
+		tab_files.append((1, mapped))
+	elif size == 0:
+		tab_files.append((0, mapped))
+	elif size == 1:
+		tab_files.append((1, mapped))
+		tab_files.append((0, os.path.join(mapped, roman.convert(tab_names[0]))))
+	else:
+		tab_files.append((1, mapped))
+		tab_files.append((2, mapped))
+		for name in tab_names:
+			tab_files.append((0, os.path.join(mapped, roman.convert(name))))
+
+	return mapped, stem, tab_files
 
--- v10/script/main.py
+++ v10/script/main.py
@@ -45,38 +45,9 @@ for dep_file in dep_files:
 six_dirs  = {}
 php_names = []
 
-def parse_dep_file (dep_file):
-
-	with open(dep_file, 'r') as f:
-		jump, sources, tab_names = eval(f.read())
-
-	tab_files = []
-
-	stem = re.sub(r'build/(.*)\.dep', r'\1', dep_file)
-	mapped = '%s' % mapper.get('map.py', os.path.dirname(stem))
-	basename = os.path.basename(stem)
-	if basename != 'index':
-		mapped = os.path.join(mapped, roman.convert(basename))
-
-	size = len(tab_names)
-	if jump:
-		tab_files.append((1, mapped))
-	elif size == 0:
-		tab_files.append((0, mapped))
-	elif size == 1:
-		tab_files.append((1, mapped))
-		tab_files.append((0, os.path.join(mapped, roman.convert(tab_names[0]))))
-	else:
-		tab_files.append((1, mapped))
-		tab_files.append((2, mapped))
-		for name in tab_names:
-			tab_files.append((0, os.path.join(mapped, roman.convert(name))))
-
-	return mapped, stem, tab_files
-
 for dep_file in dep_files:
 
-	mapped, stem, tab_files = parse_dep_file(dep_file)
+	mapped, stem, tab_files = build.parse_dep_file(dep_file)
 
 	six_dirs[mapped] = stem
 	php_names += tab_files
