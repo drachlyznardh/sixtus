2fe4027c1dcefd1cded52cd1350abd6c1ecbf648
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-26 10:24:13 +0100

    Getting names

--- v10/script/main.py
+++ v10/script/main.py
@@ -24,6 +24,8 @@ class Sixtus:
 		self.location['pag'] = 'src'
 		self.location['blog'] = 'blog'
 		self.location['build'] = 'build'
+		self.location['dep'] = 'build/dep'
+		self.location['six'] = 'build/six'
 		self.location['deploy'] = '/opt/web/mobile'
 
 		self.files = {key:[] for key in 'pag Six dep six php'.split()}
@@ -40,6 +42,9 @@ class Sixtus:
 		self.bundles = []
 
 		self.sources = {}
+
+	def get_dep_filename (self, name):
+		return os.path.join(self.location['dep'], '%s.dep' % name)
 
 	def get_six_filename (self, bundle):
 		extension = ['page.six', 'jump.six', 'side.six']
@@ -257,6 +262,10 @@ class Sixtus:
 	def build (self):
 		#self.load_wave_one()
 		self.find_page_sources()
+
+		for stem in self.sources['page']:
+			print('%15s → %30s' % (stem, self.get_dep_filename(stem)))
+
 		self.build_wave_one()
 		self.load_wave_two()
 		self.build_wave_two()
