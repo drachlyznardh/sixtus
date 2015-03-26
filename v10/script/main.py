621a002e48dd463d32017b538e775af004702e0b
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-26 14:26:04 +0100

    Building Six files

--- v10/script/main.py
+++ v10/script/main.py
@@ -12,6 +12,7 @@ import mapper
 import roman
 
 # Builders
+import Six
 import dep
 
 class Sixtus:
@@ -78,6 +79,12 @@ class Sixtus:
 	def load_Six_files (self):
 		self.files['Six'] += [self.match['pag'].sub(self.replace['Six'], name) for name in self.files['pag']]
 		if self.debug.get('list', False): print('Files[Six] = %s' % self.files['Six'])
+
+	def build_Six_file (self, stem):
+		pag_file = self.get_pag_filename(stem)
+		Six_file = self.get_Six_filename(stem)
+		#self.sources[stem] = Six.from_pag_to_Six_file(pag_file, Six_file)
+		Six.from_pag_to_Six_file(pag_file, Six_file)
 
 	def update_Six_file (self, stem):
 		Six_file = self.get_Six_filename(stem)
