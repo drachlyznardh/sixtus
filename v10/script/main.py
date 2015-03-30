d74ea9c0f80f8ed597b323f18422c59b4b2075a8
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-30 14:49:16 +0200

    New builder

--- v10/script/main.py
+++ v10/script/main.py
@@ -2,8 +2,11 @@
 # encoding: utf-8
 
 from __future__ import print_function
+
+from runtime import Runtime
 from sixtus_pages import Sixtus as Pages
 
 print('Siχtus 0.10')
+Runtime().build()
 Pages().build()
 print('Siχtus 0.10, done')
