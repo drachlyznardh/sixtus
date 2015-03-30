d97d53d499aa9eb6a70942e2ed2d642662d8a950
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-30 11:29:48 +0200

    Cleaning

--- v10/script/util.py
+++ v10/script/util.py
@@ -6,34 +6,6 @@ import os
 import re
 import fnmatch
 
-def find_all_dirs (source):
-
-	found = []
-
-	for each in os.listdir(source):
-		child = os.path.join(source, each)
-		if os.path.isdir(child):
-			found.append(child)
-			found += find_all_dirs(child)
-
-	return found
-
-def find_all_files (root_dir, pattern):
-
-	if not os.path.exists(root_dir):
-		print('Specified root_dir [%s] does not exist!' % root_dir, file=sys.stderr)
-		sys.exit(1)
-
-	visit = [root_dir] + find_all_dirs(root_dir)
-	all_files = []
-
-	for d in visit:
-		for f in os.listdir(d):
-			if fnmatch.fnmatch(f, pattern):
-				all_files.append(os.path.join(d,f))
-
-	return all_files
-
 def find_all_sources (root_dir, pattern):
 
 	if not os.path.exists(root_dir):
