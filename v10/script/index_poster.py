6fd36aed31e8acd3ec5bae81f5df9368f0d6759c
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-31 17:38:24 +0200

    One

--- v10/script/index_poster.py
+++ v10/script/index_poster.py
@@ -2,12 +2,12 @@
 # -*- encoding: utf-8 -*-
 
 from __future__ import print_function
-import sys
 
-if len(sys.argv) != 3:
-	params = ['<output file>', '<rel file>']
-	print('Usage: %s %s' % (sys.argv[0], ' '.join(params)))
-	sys.exit(1)
+class Poster:
+
+	def __init__ (self, home):
+
+		self.home = home
 
 pag_file = sys.argv[1]
 
