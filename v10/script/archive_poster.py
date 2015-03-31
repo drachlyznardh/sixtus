12370a8d7b9289a2a69b073b4e195ca5b2735424
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-31 17:15:48 +0200

    First half

--- v10/script/archive_poster.py
+++ v10/script/archive_poster.py
@@ -2,25 +2,20 @@
 # -*- encoding: utf-8 -*-
 
 from __future__ import print_function
-import sys
 
-if len(sys.argv) < 2:
-	params = ['<output file>', '<blog name file>', '<year list files…>']
-	print('Usage: %s %s' % (sys.argv[0], ' '.join(params)))
-	sys.exit(1)
+class Poster:
 
-pag_file  = sys.argv[1]
-name_file = sys.argv[2]
+	def __init__ (self, title, subtitle):
 
-with open(name_file, 'r') as f:
-	name_map = eval(f.read())
+		self.title = title
+		self.subtitle = subtitle
 
-page = []
-side = []
-for filename in sys.argv[3:]:
-	side.append(filename.split('/')[-1].split('.')[0])
-	with open(filename, 'r') as f:
-		page.append(f.read().strip())
+	def parse_files (self, list_files):
+
+		for stem in list_files:
+			self.side.append(stem[0])
+			with open(stem[1], 'r') as f:
+				self.page.append(f.read().strip())
 
 with open(pag_file, 'w') as f:
 
