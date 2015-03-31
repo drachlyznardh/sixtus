9ed9cc4fc6f4e0c349b89989884a6bf198277e4f
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-03-31 17:46:39 +0200

    HurrDurr

--- v10/script/index_poster.py
+++ v10/script/index_poster.py
@@ -9,11 +9,11 @@ class Poster:
 
 		self.home = home
 
-	def parse_target (targetlist):
+	def parse_target (self, targetlist):
 
 		self.target = '%s/%s' % (targetlist[-1])
 
-	def output_pag_file (pag_file):
+	def output_pag_file (self, pag_file):
 
 		content = 'jump|%s/%s/' % (self.home, self.target)
 
