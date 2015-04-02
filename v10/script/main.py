81a54acd401dbc9ee3c98010e9d580d0efe71c64
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-02 19:04:27 +0200

    Collecting directories with no index

--- v10/script/main.py
+++ v10/script/main.py
@@ -8,9 +8,65 @@ from resources import Resources
 from blog import Blog
 from pages import Pages
 
+import os
+
+class Filler:
+
+	def __init__ (self):
+
+		with open('conf.py', 'r') as f:
+			conf = eval(f.read())
+
+		self.location = conf.get('location')
+
+		with open('map.py', 'r') as f:
+			sitemap = eval(f.read())
+
+		self.match = sorted(sitemap.values())
+
+	def get_all_dirs (self, root):
+
+		result = []
+
+		if not os.path.exists(root):
+			raise Exception('root dir %s does not exist' % root)
+
+		if not os.path.isdir(root):
+			raise Exception('root dir %s is not a directory' % root)
+
+		visit = os.listdir(root)
+
+		while len(visit):
+			name = visit.pop(0)
+			#for name in visit:
+			dirname = os.path.join(root, name)
+			if os.path.isdir(dirname) and name[0] != '.' and name != 'sixtus':
+				content = os.listdir(dirname)
+				if 'index.php' not in content:
+					result.append(name)
+				for i in content: visit.append(os.path.join(name,i))
+				#print('Found name %s' % name)
+				#print('%s contains %s' % (dirname, os.listdir(dirname)))
+
+		return result
+
+	def build (self):
+
+		#for match in sorted(self.match):
+		#	print('Match (%s)' % match)
+
+		for name in self.get_all_dirs(self.location.get('deploy')):
+			#print(name)
+			for match in self.match:
+				#if name in match:
+				if match.startswith(name):
+					print ('%s → %s' % (name, match))
+					break
+
 print('Siχtus 0.10')
 Runtime().build()
 Resources().build()
 Blog().build()
 Pages().build()
+Filler().build()
 print('Siχtus 0.10, done')
