ab71d18fbf121d27f1004d37430dd41a51b17467
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-03 16:50:00 +0200

    Parsing options

--- v10/python/main.py
+++ v10/python/main.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function
 import sys
+import getopt
 
 from runtime import Runtime
 from resources import Resources
@@ -26,14 +27,50 @@ def sixtus_clean ():
 def sixtus_veryclean ():
 	pass
 
+def sixtus_help ():
+	print('usage: %s [options] <target…>')
+	print(' -h --help : show this help')
+
+def sixtus_version ():
+	print('Siχtus 0.10')
+
 def sixtus_read_args ():
 
-	if len(sys.argv) == 1:
+	debug = {}
+	short_opt = 'hvxwn'
+	long_opt = ['help', 'verbose', 'version',
+		'explain', 'why', 'not', 'why-not']
+
+	try: optlist, args = getopt.gnu_getopt(sys.argv[1:], short_opt, long_opt)
+	except getopot.GetoptError as err:
+		sixtus_help()
+		raise err
+
+	for key, value in optlist:
+		if key in ('-h', '--help'):
+			sixtus_help()
+			return
+		elif key in ('-v', '--verbose'):
+			debug['loud'] = True
+		elif key in ('--version'):
+			sixtus_version()
+			return
+		elif key in ('-x', '--explain'):
+			debug['why'] = True
+			debug['not'] = True
+		elif key in ('-w', '--why'):
+			debug['why'] = True
+		elif key in ('-n', '--not', '--why-not'):
+			debug['not'] = True
+
+	print(debug)
+
+	if len(args) == 0:
 		print('No args: building')
 		sixtus_build()
 		return
 
-	for target in sys.argv[1:]:
+	for target in args:
 		print('Target %s' % target)
 		if target == 'build': sixtus_build()
 		elif target == 'clean': sixtus_clean()
