cd5650b3765fee3b8df0c99274da8094e837f7cc
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-09 15:01:54 +0200

    Removing Siχtus v9

--- runtime/page/bottom.php
+++
@@ -1,7 +0,0 @@
-			</div>
-			<div id="left-side">
-<?php require_once('left-side.php'); ?>
-			</div>
-		</div>
-	</body>
-</html>
--- runtime/page/middle.php
+++
@@ -1,7 +0,0 @@
-				</div> <!-- Core -->
-				<div id="foot">
-<?php require_once('foot.php'); ?>
-				</div> <!-- Foot -->
-			</div>
-			<div id="right-side">
-<?php require_once('related.php'); ?>
