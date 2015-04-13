4df6ab04fd4cf5b39ee8ac81b303aa3735cdd6ab
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-13 17:30:36 +0200

    Distribution stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='sixtus',
+	version='0.10.2',
+	author='Ivan Simonini',
+	author_email='drachlyznardh@gmail.com',
+	package_dir={'sixtus':''},
+	packages=['sixtus'],
+	package_data={'sixtus':['data/*']}
+)
