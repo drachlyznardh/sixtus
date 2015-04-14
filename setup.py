531ffa5467812948dda2a8e5e14e9d870e45e081
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-14 15:58:58 +0200

    Bumping version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
 	name='sixtus',
-	version='0.10.5',
+	version='0.10.6',
 	url='https://github.com/drachlyznardh/sixtus',
 	author='Ivan Simonini',
 	author_email='drachlyznardh@gmail.com',
