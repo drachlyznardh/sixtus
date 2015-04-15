d519ccfb804b0c63f10b69b169949af78917d515
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-15 15:54:59 +0200

    Loader

--- index_poster.py
+++ index_poster.py
@@ -17,6 +17,49 @@ class Helper:
 
 		self.content = ''
 
+	def append_content (self, text):
+		if len(self.content):
+			self.content += '\n%s' % text
+		else: self.content = text
+
+	def parse_file (self, filename):
+		lineno = 0
+		for i in open(filename, 'r'):
+			lineno += 1
+			line = i.strip()
+
+			if len(line) == 0:
+				self.append_content(line)
+				continue
+
+			if line.startswith('#'):
+				self.append_content('')
+				continue
+
+			if line.startswith('post|'):
+				token = line.split('|')
+				size = len(token)
+
+				if size < 3:
+					print('On file %s at line %d: %s' % (filename, lineno, i))
+					raise Exception('A post must have a number and a title')
+
+				self.store_content()
+				self.current = token[1]
+
+				if size == 4:
+					self.append_content('/ %s' % token[3].capitalize())
+				if size > 4:
+					self.append_content('/ %s &amp; %s' % (', '.join(w.capitalize() for w in token[3:-1]), token[-1].capitalize()))
+
+				self.append_title(token[1], token[2])
+				self.append_content('title|%s' % token[2])
+				continue
+
+			self.append_content(line)
+
+		self.store_content()
+
 class Poster:
 
 	def __init__ (self, home):
