9c91e94f80888c98464d1542bfa9e23321e5996d
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-16 20:46:17 +0200

    MIME type and encoding

--- news_poster.py
+++ news_poster.py
@@ -107,7 +107,8 @@ class Poster:
 
 	def output_feed_file (self, filename):
 
-		output = '<?xml version="1.0" encoding="utf-8"?>\n'
+		output = '<?php header("Content-Type: application/xml; charset=utf-8"); ?>'
+		output += '<?xml version="1.0" encoding="utf-8"?>\n'
 		output += '<rss version="2.0">\n'
 		output += '\t<channel>\n'
 
