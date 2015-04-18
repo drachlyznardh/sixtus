6be4098836961a3e42366bc4917c8f95a2f44bcd
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-18 21:00:46 +0200

    Adding copyright information

--- samples/simple-no-blog/conf.py
+++ samples/simple-no-blog/conf.py
@@ -1,6 +1,8 @@
 {
 'author':{'name':'Firstname Lastname',
 	'mail':'firstname.lastname@example.com'},
+'copyright':{'owner':'Firstname Lastname',
+	'years':'200X-20XX'},
 'location':{
 	'pag':'src',
 	'build':'build',
