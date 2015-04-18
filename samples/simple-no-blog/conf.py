60e7fefd57bd542d8c9361d2978634cd39612218
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-18 21:05:57 +0200

    Adding relations

--- samples/simple-no-blog/conf.py
+++ samples/simple-no-blog/conf.py
@@ -8,5 +8,15 @@
 	'build':'build',
 	'deploy':'deploy'
 	},
-'side':'<div><p>This is a sample site</p></div>'
+'side':'<div><p>This is a sample site</p></div>',
+'lang':{
+	'page':{
+		'prev':{'body':'Link to next page @LINK@'},
+		'next':{'body':'Link to previous page @LINK@'}
+		},
+	'tab':{
+		'prev':{'body':'Link to next tab @LINK@'},
+		'next':{'body':'Link to previous tab @LINK@'}
+		}
+	}
 }
