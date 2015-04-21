a69928d1cc99d2e9fd8f27b6bfd1d6a66d612b63
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-21 10:33:56 +0200

    Copying blog example as blog-empty

---
+++ samples/blog-empty/conf.py
@@ -0,0 +1,31 @@
+{
+'author':{'name':'Firstname Lastname',
+	'mail':'firstname.lastname@example.com'},
+'copyright':{'owner':'Firstname Lastname',
+	'years':'200X-20XX'},
+'location':{
+	'blog':'blog',
+	'pag':'src',
+	'build':'build',
+	'deploy':'deploy'
+	},
+'side':'<div><p>This is the left side.</p></div>',
+'lang':{
+	'page':{
+		'prev':{'body':'<div id="pag_prev" class="inside"><p>/ @LINK@ / Previous</p></div>'},
+		'next':{'body':'<div id="pag_next" class="outside"><p class="reverse">Following / @LINK@ /</p></div>'}
+		},
+	'tab':{
+		'prev':{'body':''},
+		'next':{'body':''}
+		},
+	'blog':{
+		'news':{'title':'News title',
+			'subtitle':'News subtitle',
+			'threshold':10},
+		'archive':{'title':'Archive title',
+			'subtitle':'Archive subtitle'}
+		},
+	'month':{}
+	},
+}
