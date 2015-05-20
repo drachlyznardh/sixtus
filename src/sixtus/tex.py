88a91d298f56cd5f606a47464d48e2c56fa6ee50
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-05-20 17:00:53 +0200

    Searching

--- src/sixtus/tex.py
+++ src/sixtus/tex.py
@@ -3,6 +3,32 @@
 from __future__ import print_function
 
 import os
+import re
+
+def _search_for_targets (root):
+
+	if not os.path.exists(root):
+		raise Exception('Root dir %s does not exist' % root)
+
+	if not os.path.isdir(root):
+		raise Exception('Root %s is not a directory' % root)
+
+	pattern = re.compile(r'.*\.(pag|six)')
+
+	result = []
+	visit = ['']
+	while len(visit):
+		this_dir = visit.pop(0)
+		this_path = os.path.join(root, this_dir)
+		for item in sorted(os.listdir(this_path)):
+			item_path = os.path.join(this_path, item)
+			if os.path.isdir(item_path):
+				result.append((0, item_path))
+				visit.append(os.path.join(this_dir, item))
+			elif pattern.match(item):
+				result.append((1, item_path))
+
+	return result
 
 class Tex:
 
@@ -16,3 +42,6 @@ class Tex:
 		else:
 			root = os.getcwd()
 			print('Empty target, using current dir %s' % root)
+			target_list = _search_for_targets(root)
+			for each in target_list: print(each)
+
