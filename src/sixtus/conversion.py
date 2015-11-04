bfe8f720256ae1e7a481e8841c582b73347ce8eb
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-11-04 14:36:05 +0100

    Cleaning

--- src/sixtus/conversion.py
+++ src/sixtus/conversion.py
@@ -226,15 +226,9 @@ class Content:
 			return self.do_make_list(env, 0, 0)
 
 		if env in ('title', 'stitle'):
-			size = len(args)
-			align = 'left'
-			if size > 1:
-				#if args[1] in ('', 'left', 'center', 'right'):
-				#	align = args[1]
-				#if size > 2:
-				self.error('Too many args for %s block' % env)
-
-			return self.do_make_title_block(env, align)
+			if len(args) > 1:
+				self.error('Too many args for %s block, %s' % (env, args))
+			return self.do_make_title_block(env)
 
 		if env in ('ol', 'dl'):
 			size = len(args)
