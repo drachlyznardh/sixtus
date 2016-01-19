838072c5197dfcb4ed1e54d9a1280722c654d046
Ivan Simonini <drachlyznardh@roundhousecode.com>
2016-01-19 16:33:01 +0100

    Terrible experiments

--- src/sixtus/data/panel.js
+++ src/sixtus/data/panel.js
@@ -32,3 +32,20 @@ var makeTidVisible = function () {
 	if (rtid > rdiv) div.scrollTop = rtid - rdiv;
 	return;
 }
+
+function adjustHeight () {
+	var relation = document.getElementById('relations');
+	var relstyle = window.getComputedStyle(relation);
+	var h = document.getElementById('right-side-panel').clientHeight
+	var sliding = document.getElementById('sliding-content');
+	var slistyle = window.getComputedStyle(sliding);
+	h -= relation.offsetHeight + parseFloat(relstyle.marginTop) + parseFloat(relstyle.marginBottom);
+	h -= parseFloat(slistyle.marginTop) + parseFloat(slistyle.marginBottom) + parseFloat(slistyle.paddingTop) + parseFloat(slistyle.paddingBottom);
+	//alert(style);
+	//alert(document.getElementById('right-side-panel').clientHeight);
+	//alert(relation.offsetHeight);
+	//alert(style.marginTop);
+	//alert(style.marginBottom);
+	//alert(h);
+	sliding.style.height = h.toString().concat('px');
+}
